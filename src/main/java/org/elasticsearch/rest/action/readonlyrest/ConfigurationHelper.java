b75fae2d6f0c2d5b4c62ca29e993108b827e8607
sscarduzio <scarduzio@gmail.com>
2013-12-08 17:23:59 +0100

    Amend terrible formatting

--- src/main/java/org/elasticsearch/rest/action/readonlyrest/ConfigurationHelper.java
+++ src/main/java/org/elasticsearch/rest/action/readonlyrest/ConfigurationHelper.java
@@ -12,78 +12,82 @@ import org.elasticsearch.common.settings.Settings;
  * ConfigurationHelper
  * 
  * @author <a href="mailto:scarduzio@gmail.com">Simone Scarduzio</a>
- * @see <a href="https://github.com/sscarduzio/elasticsearch-readonlyrest-plugin/">Github page</a>
+ * @see <a href="https://github.com/sscarduzio/elasticsearch-readonlyrest-plugin/">Github Project</a>
  */
 public class ConfigurationHelper {
 
-	/**
-	 * YML prefix of this plugin inside elasticsearch.yml
-	 */
-	private final static String	ES_YML_CONF_PREFIX	= "readonlyrest.";
+  /**
+   * YML prefix of this plugin inside elasticsearch.yml
+   */
+  private final static String ES_YML_CONF_PREFIX = "readonlyrest.";
 
-	/**
-	 * Maps to "allow_localhost" in elasticsearch.yml
-	 */
-	private static Boolean			allowLocalhost;
+  /**
+   * Maps to "allow_localhost" in elasticsearch.yml
+   */
+  private static Boolean      allowLocalhost;
 
-	/**
-	 * Maps to "whitelist" in elasticsearch.yml
-	 */
-	private static Set<String>	whitelist;
+  /**
+   * Maps to "whitelist" in elasticsearch.yml
+   */
+  private static Set<String>  whitelist;
 
-	/**
-	 * Maps to "forbidden_uri_re" in elasticsearch.yml
-	 */
-	private static Pattern			forbiddenUriRe			= null;
+  /**
+   * Maps to "forbidden_uri_re" in elasticsearch.yml
+   */
+  private static Pattern      forbiddenUriRe     = null;
 
-	/**
-	 * Maps to "barred_reason_string" in elasticsearch.yml
-	 */
-	private String	barredReasonString = "";
+  /**
+   * Maps to "barred_reason_string" in elasticsearch.yml
+   */
+  private String              barredReasonString = "";
 
-	public ConfigurationHelper(Settings settings, ESLogger logger) {
-		// Load configuration
-		if (!settings.getAsBoolean(ES_YML_CONF_PREFIX + "enable", false)) {
-			logger.info("Readonly Rest plugin is installed, but not enabled");
-			return;
-		}
+  public ConfigurationHelper(Settings settings, ESLogger logger) {
+    // Load configuration
+    if (!settings.getAsBoolean(ES_YML_CONF_PREFIX + "enable", false)) {
+      logger.info("Readonly Rest plugin is installed, but not enabled");
+      return;
+    }
 
-		allowLocalhost = settings.getAsBoolean(ES_YML_CONF_PREFIX + "allow_localhost", true);
-		logger.info("allowing all GET requests from: localhost");
+    allowLocalhost = settings.getAsBoolean(ES_YML_CONF_PREFIX + "allow_localhost", true);
+    logger.info("allowing all GET requests from: localhost");
 
-		String[] aTmp = settings.getAsArray(ES_YML_CONF_PREFIX + "whitelist");
-		if (aTmp.length > 0) {
-			whitelist = new HashSet<String>(Arrays.asList(aTmp));
-			logger.info("allowing all GET requests from: " + settings.get(ES_YML_CONF_PREFIX + "whitelist"));
-		}
-		String sTmp = settings.get("forbidden_uri_re");
-		if (sTmp != null && sTmp.trim().length() > 0) {
-			try {
-				forbiddenUriRe = Pattern.compile(sTmp);
-			} catch (Throwable t) {
-				logger.error("invalid regular expression provided as forbidden_uri_re: " + sTmp);
-			}
-		}
-		sTmp = settings.get("forbidden_uri_re");
-		if(sTmp != null){
-			barredReasonString = sTmp;
-		}
-	}
+    String[] aTmp = settings.getAsArray(ES_YML_CONF_PREFIX + "whitelist");
+    if (aTmp.length > 0) {
+      whitelist = new HashSet<String>(Arrays.asList(aTmp));
+      logger.info("allowing all GET requests from: " + settings.get(ES_YML_CONF_PREFIX + "whitelist"));
+    }
+    String sTmp = settings.get("forbidden_uri_re");
+    if (sTmp != null && sTmp.trim().length() > 0) {
+      try {
+        forbiddenUriRe = Pattern.compile(sTmp);
+      }
+      catch (Throwable t) {
+        logger.error("invalid regular expression provided as forbidden_uri_re: " + sTmp);
+      }
+    }
+    sTmp = settings.get("forbidden_uri_re");
+    if (sTmp != null) {
+      barredReasonString = sTmp;
+    }
+  }
+  
+  /*
+   * Getters
+   */
+  public Boolean isAllowLocalhost() {
+    return allowLocalhost;
+  }
 
-	public Boolean isAllowLocalhost() {
-		return allowLocalhost;
-	}
+  public Set<String> getWhitelist() {
+    return whitelist;
+  }
 
-	public Set<String> getWhitelist() {
-		return whitelist;
-	}
+  public Pattern getForbiddenUriRe() {
+    return forbiddenUriRe;
+  }
 
-	public Pattern getForbiddenUriRe() {
-		return forbiddenUriRe;
-	}
-
-	public String getBarredReasonString() {
-		return barredReasonString;
-	}
+  public String getBarredReasonString() {
+    return barredReasonString;
+  }
 
 }
