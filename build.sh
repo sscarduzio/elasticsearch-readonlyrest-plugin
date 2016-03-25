65283e47cebdcca90a2dfa8c9ce8c9844c4972d1
Simone Scarduzio <scarduzio@gmail.com>
2016-03-25 18:27:58 +0000

    automatically update the property file

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 mvn clean package
+
+ES_VERSION=`grep '<elasticsearch.version' pom.xml | sed 's|</b>|-|g' | sed 's|<[^>]*>||g' |xargs`
+
+sed -i.bak "s/^\(elasticsearch.version=\).*/\1$ES_VERSION/" src/main/resources/plugin-descriptor.properties
+
 zip -j -g target/elasticsearch-readonlyrest-v*es-v2*.zip src/main/resources/plugin-descriptor.properties
 zip -j -g target/elasticsearch-readonlyrest-v*es-v2*.zip src/main/resources/plugin-security.policy
