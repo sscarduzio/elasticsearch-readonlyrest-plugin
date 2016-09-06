503557612d68170e84ecff5e454f9c4965fb2d14
Simone Scarduzio <scarduzio@gmail.com>
2016-09-06 17:07:42 +0200

    checksum support

--- build.sh
+++ build.sh
@@ -13,5 +13,9 @@ ES_VERSION=`grep '<elasticsearch.version' pom.xml | sed 's|</b>|-|g' | sed 's|<[
 sed  -i.bak  "s/^\(elasticsearch.version=\).*/\1$ES_VERSION/" src/main/resources/plugin-descriptor.properties && rm src/main/resources/plugin-descriptor.properties.bak
 sed  -i.bak  "s/^\(elasticsearch.version=\).*/\1$ES_VERSION/" src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties && rm src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties.bak
 
-zip -j -g target/elasticsearch-readonlyrest-v*es-v2*.zip src/main/resources/plugin-descriptor.properties
-zip -j -g target/elasticsearch-readonlyrest-v*es-v2*.zip src/main/resources/plugin-security.policy
+PACK=`ls target/elasticsearch-readonlyrest-v*es-v2*.zip`
+
+zip -j -g $PACK src/main/resources/plugin-descriptor.properties
+zip -j -g $PACK src/main/resources/plugin-security.policy
+
+shasum -a1 $PACK > $PACK.sha1
