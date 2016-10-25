1bcc55459e5c906c04f1fe055d5c2778050d3e27
Simone Scarduzio <scarduzio@gmail.com>
2016-10-25 19:53:46 +0200

    update descriptors first, then package + missing plugin version

--- build.sh
+++ build.sh
@@ -6,14 +6,20 @@ then
     sed -i.bak 's|<elasticsearch.version>.*</elasticsearch.version>|<elasticsearch.version>'"${1}"'</elasticsearch.version>|g'  pom.xml && rm pom.xml.bak
 fi
 
-mvn clean package || exit 1
-
+PLUGIN_VERSION=`grep '<!-- plugin_version' pom.xml | sed 's|</b>|-|g' | sed 's|<[^>]*>||g' |xargs`
 ES_VERSION=`grep '<elasticsearch.version' pom.xml | sed 's|</b>|-|g' | sed 's|<[^>]*>||g' |xargs`
 
 sed  -i.bak  "s/^\(elasticsearch.version=\).*/\1$ES_VERSION/" src/main/resources/plugin-descriptor.properties && rm src/main/resources/plugin-descriptor.properties.bak
+sed  -i.bak  "s/^\(version=\).*/\1$PLUGIN_VERSION/" src/main/resources/plugin-descriptor.properties && rm src/main/resources/plugin-descriptor.properties.bak
+
 sed  -i.bak  "s/^\(elasticsearch.version=\).*/\1$ES_VERSION/" src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties && rm src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties.bak
+sed  -i.bak  "s/^\(version=\).*/\1$PLUGIN_VERSION/" src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties && rm src/test/eshome/plugins/readonlyrest/plugin-descriptor.properties.bak
 
 PACK=`ls target/elasticsearch-readonlyrest-v*es-v2*.zip`
+
+# -Dmaven.test.skip=true -DskipTests
+mvn clean package || exit 1
+
 
 zip -j -g $PACK src/main/resources/plugin-descriptor.properties
 zip -j -g $PACK src/main/resources/plugin-security.policy
