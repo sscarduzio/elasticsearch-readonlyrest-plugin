0f65eed651e56204f4b24fb512aac008df8d0f9e
Simone Scarduzio <scarduzio@gmail.com>
2017-01-12 23:18:17 +0700

    fix uploading the whole plugin zip instead of the checksum..

--- ci/ci-deploy.sh
+++ ci/ci-deploy.sh
@@ -66,7 +66,7 @@ EOM
 
 # s3cli -c config.json  put <path/to/file> <remote-blob>
 $S3CLI  -c $CONF_FILE   put $PLUGIN_FILE   build/$PLUGIN_VERSION/$PLUGIN_FILE_BASE
-$S3CLI  -c $CONF_FILE   put $PLUGIN_FILE   build/$PLUGIN_VERSION/$PLUGIN_FILE_BASE.sha1
+$S3CLI  -c $CONF_FILE   put $PLUGIN_FILE.sha1   build/$PLUGIN_VERSION/$PLUGIN_FILE_BASE.sha1
 
 rm $CONF_FILE
 
