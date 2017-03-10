97f0d7cf96f6421d2b70e9756a04f35d9861a021
Simone Scarduzio <scarduzio@gmail.com>
2017-03-10 16:12:55 +0100

    proxy_auth now supports wildcard matching

--- src/main/java/org/elasticsearch/plugin/readonlyrest/acl/blocks/rules/impl/ProxyAuthSyncRule.java
+++ src/main/java/org/elasticsearch/plugin/readonlyrest/acl/blocks/rules/impl/ProxyAuthSyncRule.java
@@ -17,17 +17,18 @@
 
 package org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.impl;
 
-import com.google.common.collect.Lists;
-import java.util.List;
-import java.util.Map;
-
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugin.readonlyrest.acl.RequestContext;
+import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.MatcherWithWildcards;
 import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.RuleExitResult;
 import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.RuleNotConfiguredException;
 import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.SyncRule;
 import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.UserRule;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by ah on 15/02/2016.
@@ -35,24 +36,22 @@ import org.elasticsearch.plugin.readonlyrest.acl.blocks.rules.UserRule;
 public class ProxyAuthSyncRule extends SyncRule implements UserRule {
 
   private static final String HEADER = "X-Forwarded-User";
-  private List<String> userList;
-  
+  private final MatcherWithWildcards userListMatcher;
+
   public ProxyAuthSyncRule(Settings s) throws RuleNotConfiguredException {
     super();
     String[] users = s.getAsArray(getKey());
     if (users != null && users.length > 0) {
-      userList = Lists.newArrayList();
-      for (int i = 0; i < users.length; i++) {
-        if (!Strings.isNullOrEmpty(users[i])) {
-          userList.add(users[i].trim());
-        }
-      }
+      userListMatcher = new MatcherWithWildcards(Arrays.stream(users)
+                                                   .filter(i -> !Strings.isNullOrEmpty(i))
+                                                   .distinct()
+                                                   .collect(Collectors.toSet()));
     }
     else {
       throw new RuleNotConfiguredException();
     }
   }
-  
+
   public static String getUser(Map<String, String> headers) {
     String h = headers.get(HEADER);
     if (h == null || h.trim().length() == 0)
@@ -63,25 +62,16 @@ public class ProxyAuthSyncRule extends SyncRule implements UserRule {
   @Override
   public RuleExitResult match(RequestContext rc) {
     String h = getUser(rc.getHeaders());
-    
+
     if (h == null) {
       return NO_MATCH;
     }
-    
+
     if (h.length() == 0) {
       return NO_MATCH;
     }
 
-    for(String user: userList) {
-      if ("*".equals(user)) {
-        return MATCH;
-      }
-      if (user.equals(h)) {
-        return MATCH;
-      }
-    }
-  
-    return NO_MATCH;
+    return userListMatcher.match(h) ? MATCH : NO_MATCH;
   }
-  
+
 }
