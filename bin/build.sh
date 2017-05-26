8f1f84994ebfb4083d4bd974c2371859f4d2a6e9
Simone Scarduzio <scarduzio@gmail.com>
2017-05-26 16:48:00 +0300

    debug info

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./gradlew --stacktrace test ror &&
+./gradlew --stacktrace --debug test ror &&
 # additional build of ES module for specified ES version
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
+./gradlew --stacktrace --debug es53x:ror '-PesVersion=5.3.0'
