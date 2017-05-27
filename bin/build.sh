a176634541abdaafd00e066e6dae3cf15ddf804c
Simone Scarduzio <scarduzio@gmail.com>
2017-05-27 11:54:43 +0300

    remove gradle progress logs

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Log file friendly Gradle output
+export TERM=dumb
+
 ./gradlew --stacktrace --debug test ror &&
 # additional build of ES module for specified ES version
 ./gradlew --stacktrace --debug es53x:ror '-PesVersion=5.3.0'
