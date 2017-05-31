f77ea83b5f53ae8f61124e72202fc2cf67eec486
Simone Scarduzio <scarduzio@gmail.com>
2017-05-31 14:41:38 +0300

    build more versions

--- bin/build.sh
+++ bin/build.sh
@@ -10,4 +10,9 @@ done
 
 ./gradlew --stacktrace --debug test ror &&
 # additional build of ES module for specified ES version
-./gradlew --stacktrace --debug es53x:ror '-PesVersion=5.3.0'
+./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
+./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
+./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
+./gradlew --stacktrace es52x:ror '-PesVersion=5.2.2'
+./gradlew --stacktrace es52x:ror '-PesVersion=5.2.1'
+./gradlew --stacktrace es52x:ror '-PesVersion=5.2.0'
