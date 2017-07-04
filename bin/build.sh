944cbc87fd236328f0fdbfff4e7f30ac577a038e
Simone Scarduzio <scarduzio@gmail.com>
2017-07-04 09:43:01 +0200

    disable for now 5.3.x

--- bin/build.sh
+++ bin/build.sh
@@ -13,9 +13,9 @@ done
 
 ./gradlew --stacktrace --debug test ror &&
 # additional build of ES module for specified ES version
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
+# ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
+#./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
+#./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.0'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.1'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.2'
