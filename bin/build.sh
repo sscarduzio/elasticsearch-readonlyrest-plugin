836094aaf124c1512a551be8e1e30926c822ac09
Simone Scarduzio <scarduzio@gmail.com>
2017-07-05 15:29:58 +0200

    docker image for 2.x needs work..

--- bin/build.sh
+++ bin/build.sh
@@ -6,9 +6,10 @@
 # Log file friendly Gradle output
 export TERM=dumb
 
-for esProject in `ls | grep 'es\d\dx'`; do
+#for esProject in `ls | grep 'es\d\dx'`; do
+for esProject in `ls | grep 'es5\dx'`; do
     echo "Running integration tests for $esProject ..."
-    ./gradlew integration-tests:test '-PesModule='$esProject --debug
+    ./gradlew integration-tests:test '-PesModule='$esProject # --debug
 done
 
 ./gradlew --stacktrace --debug test ror &&
@@ -16,6 +17,10 @@ done
 # ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
 #./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
 #./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
+
+./gradlew --stacktrace es23x:ror '-PesVersion=2.4.5'
+./gradlew --stacktrace es23x:ror '-PesVersion=2.4.1'
+
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.0'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.1'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.2'
