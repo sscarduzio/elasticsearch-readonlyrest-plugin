ebf3ac8e55047cfce8587fcbf0ae87c725785beb
Simone Scarduzio <scarduzio@gmail.com>
2017-07-07 00:52:14 +0200

    loggy build script to see better what’s going on

--- bin/build.sh
+++ bin/build.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
-# Check all license headers are in place
-./gradlew license
+echo "####################################"
+echo "($0) RUNNINIG CONTINUOUS INTEGRATION"
+echo "####################################"
 
 # Log file friendly Gradle output
 export TERM=dumb
 
+echo -n "Check all license headers are in place"
+./gradlew license
+
 #for esProject in `ls | grep 'es\d\dx'`; do
 for esProject in `ls | grep 'es5\dx'`; do
     echo "Running integration tests for $esProject ..."
-    ./gradlew integration-tests:test '-PesModule='$esProject # --debug
+    ./gradlew --stacktrace integration-tests:test '-PesModule='$esProject # --debug
 done
 
 ./gradlew --stacktrace --debug test ror &&
-# additional build of ES module for specified ES version
+
+echo "##########################################################"
+echo ($0) additional build of ES module for specified ES version
+echo "##########################################################"
+
 # ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
 #./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
 #./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
 
+#./gradlew --stacktrace es23x:ror '-PesVersion=2.4.1'
 ./gradlew --stacktrace es23x:ror '-PesVersion=2.4.5'
-./gradlew --stacktrace es23x:ror '-PesVersion=2.4.1'
 
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.0'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.1'
