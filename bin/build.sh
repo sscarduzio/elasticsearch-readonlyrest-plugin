da61bebee42e6ddc716a3dd6846e0d95e86338d1
Simone Scarduzio <scarduzio@gmail.com>
2017-07-21 07:55:35 +0200

    build for 5.1.1

--- bin/build.sh
+++ bin/build.sh
@@ -47,3 +47,5 @@ echo ">>> ($0) additional build of ES module for specified ES version"
 ./gradlew --stacktrace es52x:ror '-PesVersion=5.2.2'
 ./gradlew --stacktrace es52x:ror '-PesVersion=5.2.1'
 ./gradlew --stacktrace es52x:ror '-PesVersion=5.2.0'
+
+./gradlew --stacktrace es51x:ror '-PesVersion=5.1.1'
