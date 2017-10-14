0d2615526a6c6b8fef9c35024b8d7f1495d2067b
Simone Scarduzio <scarduzio@gmail.com>
2017-10-14 15:42:30 +0200

    left some garbage here

--- integration-tests/src/test/java/tech/beshu/ror/integration/DynamicVariablesTests.java
+++ integration-tests/src/test/java/tech/beshu/ror/integration/DynamicVariablesTests.java
@@ -16,10 +16,7 @@
  */
 package tech.beshu.ror.integration;
 
-import com.google.common.base.Charsets;
-import com.jayway.jsonpath.JsonPath;
 import com.mashape.unirest.http.Unirest;
-import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
@@ -32,8 +29,6 @@ import tech.beshu.ror.utils.containers.ESWithReadonlyRestContainer;
 import tech.beshu.ror.utils.gradle.RorPluginGradleProject;
 import tech.beshu.ror.utils.httpclient.RestClient;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -41,14 +36,7 @@ import static org.junit.Assert.assertTrue;
 
 public class DynamicVariablesTests {
 
-  private static final String NOT_EXISTS = "{\"index\":[\"perfmon_index_does_not_exist\"],\"ignore_unavailable\":true,\"preference\":1506497937939}\n" +
-    "{\"query\":{\"bool\":{\"must_not\":[{\"match_all\":{}}]}}}";
-  private static final String QUERY_WORKS = "{\"index\":[\".kibana\"],\"ignore_unavailable\":true,\"preference\":1506497937939}\n" +
-    "{\"query\":{\"match_all\":{}}, \"size\":0}";
-  private static final String EMPTY_INDEX = "{\"index\":[\".kibana\"],\"ignore_unavailable\":true,\"preference\":1506497937939}\n" +
-    "{\"query\":{\"bool\":{\"must_not\":[{\"match_all\":{}}]}}}";
   private static RestClient adminClient;
-
 
   @ClassRule
   public static ESWithReadonlyRestContainer container =
@@ -56,7 +44,7 @@ public class DynamicVariablesTests {
       RorPluginGradleProject.fromSystemProperty(),
       "/dynamic_vars/elasticsearch.yml",
       Optional.of(client -> {
-        Unirest.setHttpClient(adminClient.getUnderlyingClient());
+        Unirest.setHttpClient(client.getUnderlyingClient());
         insertDoc(".kibana_simone", client);
       })
     );
@@ -106,39 +94,6 @@ public class DynamicVariablesTests {
     assertTrue(body.contains("asd"));
   }
 
-  @Test
-  public void test274_1() throws Exception {
-    String result = msearch(NOT_EXISTS);
-    assertTrue("[0]".equals(result));
-
-    result = msearch(QUERY_WORKS);
-    assertTrue("[1]".equals(result));
-
-    result = msearch(EMPTY_INDEX);
-    assertTrue("[0]".equals(result));
-
-    result = msearch(EMPTY_INDEX + "\n" + QUERY_WORKS + "\n" + EMPTY_INDEX);
-    assertTrue("[0,1,0]".equals(result));
-
-  }
-
-
-  private String msearch(String reqBody) throws Exception {
-    String caller = Thread.currentThread().getStackTrace()[2].getMethodName();
-    RestClient userClient = container.getBasicAuthClient("pablo", "dev");
-
-    String url = userClient.from("/_msearch").toASCIIString();
-    com.mashape.unirest.http.HttpResponse<InputStream> resp = Unirest.post(url)
-      .field("x", NOT_EXISTS.getBytes(StandardCharsets.UTF_8))
-      .asBinary();
-
-
-    String body = IOUtils.toString(resp.getBody(), Charsets.UTF_8);
-    System.out.println("MSEARCH RESPONSE for " + caller + ": " + body);
-    assertEquals(200, resp.getStatus());
-    body = JsonPath.parse(body).read("$.responses[*]._shards.total").toString();
-    return body;
-  }
 
   private String search(String endpoint) throws Exception {
     String caller = Thread.currentThread().getStackTrace()[2].getMethodName();
