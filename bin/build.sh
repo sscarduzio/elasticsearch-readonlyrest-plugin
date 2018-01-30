74facfdf77841fb369830e5c385eefc0fd6de65b
Simone Scarduzio <scarduzio@gmail.com>
2018-01-30 12:54:49 -0600

    release for new ES versions

--- bin/build.sh
+++ bin/build.sh
@@ -40,7 +40,8 @@ echo ">>> ($0) additional builds of ES module for specified ES version"
 # es61
 ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.0'
 ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.1'
-#./gradlew --stacktrace es61x:ror '-PesVersion=6.1.2'
+./gradlew --stacktrace es61x:ror '-PesVersion=6.1.2'
+#./gradlew --stacktrace es61x:ror '-PesVersion=6.1.3'
 
 
 # es60
@@ -55,7 +56,8 @@ echo ">>> ($0) additional builds of ES module for specified ES version"
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.3'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.4'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.5'
-#./gradlew --stacktrace es53x:ror '-PesVersion=5.6.6'
+./gradlew --stacktrace es53x:ror '-PesVersion=5.6.6'
+#./gradlew --stacktrace es53x:ror '-PesVersion=5.6.7'
 
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.0'
 ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.1'
