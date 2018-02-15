c4316e7e4f85e41581b11be73de965ff9ddbd14c
Simone Scarduzio <scarduzio@gmail.com>
2018-02-14 19:10:29 -0600

    try to parallelise jobs so it won't time out

--- bin/build.sh
+++ bin/build.sh
@@ -7,27 +7,40 @@ echo ">>> ($0) RUNNING CONTINUOUS INTEGRATION"
 # Log file friendly Gradle output
 export TERM=dumb
 
-echo  ">>> Check all license headers are in place"
-./gradlew license
 
-echo ">>> Running unit tests.."
-./gradlew --stacktrace test ror
+if [[ TRAVIS != "true" ]] ||  [[ $ROR_TASK == "license" ]]; then
+
+    echo  ">>> Check all license headers are in place"
+    ./gradlew license
+
+fi
 
 
-echo ">>> es61x => Running testcontainers.."
-./gradlew integration-tests:test '-PesModule=es61x' || ( find . |grep hs_err |xargs cat && exit 1 )
+if [[ TRAVIS != "true" ]] ||  [[ $ROR_TASK == "unit" ]]; then
 
-echo ">>> es60x => Running testcontainers.."
-./gradlew integration-tests:test '-PesModule=es60x' || ( find . |grep hs_err |xargs cat && exit 1 )
+    echo ">>> Running unit tests.."
+    ./gradlew --stacktrace test ror
 
-echo ">>> es53x => Running testcontainers.."
-./gradlew  integration-tests:test '-PesModule=es53x' || ( find . |grep hs_err |xargs cat && exit 1 )
+fi
 
-echo ">>> es52x => Running testcontainers.."
-./gradlew  integration-tests:test '-PesModule=es52x' || ( find . |grep hs_err |xargs cat && exit 1 )
+if [[ TRAVIS != "true" ]] ||  [[ $ROR_TASK == "integration" ]]; then
 
-echo ">>> es51x => Running testcontainers.."
-./gradlew  integration-tests:test '-PesModule=es51x' || ( find . |grep hs_err |xargs cat && exit 1 )
+    echo ">>> es61x => Running testcontainers.."
+    ./gradlew integration-tests:test '-PesModule=es61x' || ( find . |grep hs_err |xargs cat && exit 1 )
+
+    echo ">>> es60x => Running testcontainers.."
+    ./gradlew integration-tests:test '-PesModule=es60x' || ( find . |grep hs_err |xargs cat && exit 1 )
+
+    echo ">>> es53x => Running testcontainers.."
+    ./gradlew  integration-tests:test '-PesModule=es53x' || ( find . |grep hs_err |xargs cat && exit 1 )
+
+    echo ">>> es52x => Running testcontainers.."
+    ./gradlew  integration-tests:test '-PesModule=es52x' || ( find . |grep hs_err |xargs cat && exit 1 )
+
+    echo ">>> es51x => Running testcontainers.."
+    ./gradlew  integration-tests:test '-PesModule=es51x' || ( find . |grep hs_err |xargs cat && exit 1 )
+
+fi
 
 
 if [[ $TRAVIS_PULL_REQUEST == "true" ]] && [[ $TRAVIS_BRANCH != "master" ]]; then
@@ -35,64 +48,68 @@ if [[ $TRAVIS_PULL_REQUEST == "true" ]] && [[ $TRAVIS_BRANCH != "master" ]]; the
     exit 0
 fi
 
-echo ">>> ($0) additional builds of ES module for specified ES version"
 
-# es61
-./gradlew --stacktrace es61x:ror '-PesVersion=6.1.0'
-./gradlew --stacktrace es61x:ror '-PesVersion=6.1.1'
-./gradlew --stacktrace es61x:ror '-PesVersion=6.1.2'
-./gradlew --stacktrace es61x:ror '-PesVersion=6.1.3'
-./gradlew --stacktrace es61x:ror '-PesVersion=6.2.0'
-#./gradlew --stacktrace es61x:ror '-PesVersion=6.2.1'
+if [[ TRAVIS != "true" ]] ||  [[ $ROR_TASK == "package" ]]; then
 
+    echo ">>> ($0) additional builds of ES module for specified ES version"
 
-# es60
-./gradlew --stacktrace es60x:ror '-PesVersion=6.0.0'
-#./gradlew --stacktrace es60x:ror '-PesVersion=6.0.1'
+    # es61
+    ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.0'
+    ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.1'
+    ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.2'
+    ./gradlew --stacktrace es61x:ror '-PesVersion=6.1.3'
+    ./gradlew --stacktrace es61x:ror '-PesVersion=6.2.0'
+    #./gradlew --stacktrace es61x:ror '-PesVersion=6.2.1'
 
 
-# es53
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.0'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.1'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.2'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.3'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.4'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.5'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.6.6'
-#./gradlew --stacktrace es53x:ror '-PesVersion=5.6.7'
-
-./gradlew --stacktrace es53x:ror '-PesVersion=5.5.0'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.5.1'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.5.2'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.5.3'
-
-./gradlew --stacktrace es53x:ror '-PesVersion=5.4.0'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.4.1'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.4.2'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.4.3'
-
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
-./gradlew --stacktrace es53x:ror '-PesVersion=5.3.3'
+    # es60
+    ./gradlew --stacktrace es60x:ror '-PesVersion=6.0.0'
+    #./gradlew --stacktrace es60x:ror '-PesVersion=6.0.1'
 
 
-# es52
-./gradlew --stacktrace es52x:ror '-PesVersion=5.2.0'
-./gradlew --stacktrace es52x:ror '-PesVersion=5.2.1'
-#./gradlew --stacktrace es52x:ror '-PesVersion=5.2.2'
+    # es53
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.0'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.1'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.2'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.3'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.4'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.5'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.6'
+    #./gradlew --stacktrace es53x:ror '-PesVersion=5.6.7'
+
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.0'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.1'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.2'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.3'
+
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.0'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.1'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.2'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.4.3'
+
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.0'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.1'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.2'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.3.3'
 
 
-# es51
-./gradlew --stacktrace es51x:ror '-PesVersion=5.1.1'
-#./gradlew --stacktrace es51x:ror '-PesVersion=5.1.2'
+    # es52
+    ./gradlew --stacktrace es52x:ror '-PesVersion=5.2.0'
+    ./gradlew --stacktrace es52x:ror '-PesVersion=5.2.1'
+    #./gradlew --stacktrace es52x:ror '-PesVersion=5.2.2'
 
-# es23x
-./gradlew --stacktrace es23x:ror '-PesVersion=2.4.0'
-./gradlew --stacktrace es23x:ror '-PesVersion=2.4.1'
-#./gradlew --stacktrace es23x:ror '-PesVersion=2.4.2'
-#./gradlew --stacktrace es23x:ror '-PesVersion=2.4.3'
-./gradlew --stacktrace es23x:ror '-PesVersion=2.4.4'
-./gradlew --stacktrace es23x:ror '-PesVersion=2.4.5'
-#./gradlew --stacktrace es23x:ror '-PesVersion=2.4.6'
 
+    # es51
+    ./gradlew --stacktrace es51x:ror '-PesVersion=5.1.1'
+    #./gradlew --stacktrace es51x:ror '-PesVersion=5.1.2'
+
+    # es23x
+    ./gradlew --stacktrace es23x:ror '-PesVersion=2.4.0'
+    ./gradlew --stacktrace es23x:ror '-PesVersion=2.4.1'
+    #./gradlew --stacktrace es23x:ror '-PesVersion=2.4.2'
+    #./gradlew --stacktrace es23x:ror '-PesVersion=2.4.3'
+    ./gradlew --stacktrace es23x:ror '-PesVersion=2.4.4'
+    ./gradlew --stacktrace es23x:ror '-PesVersion=2.4.5'
+    #./gradlew --stacktrace es23x:ror '-PesVersion=2.4.6'
+
+fi
