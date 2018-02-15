4b81af7990db7ad6b5235279756ff63bd27e3ae0
Simone Scarduzio <scarduzio@gmail.com>
2018-02-14 19:36:31 -0600

    typo

--- core/src/test/java/tech/beshu/ror/settings/LdapAuthenticationRuleSettingsTests.java
+++ core/src/test/java/tech/beshu/ror/settings/LdapAuthenticationRuleSettingsTests.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 public class LdapAuthenticationRuleSettingsTests {
 
   @Test
-  public void testRuleSettginsSuccessfulCreation() {
+  public void testRuleSettingsSuccessfulCreation() {
     LdapAuthenticationRuleSettings settings = LdapAuthenticationRuleSettings.from(
       TestUtils.fromYAMLString("" +
                                  "ldap_authentication:\n" +
