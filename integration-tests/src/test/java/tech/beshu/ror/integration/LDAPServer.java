0fe9764bd125de2d3ad801b9b33438c5c440c84e
Simone Scarduzio <scarduzio@gmail.com>
2018-05-16 15:51:33 +0200

    LDAP test server

---
+++ integration-tests/src/test/java/tech/beshu/ror/integration/LDAPServer.java
@@ -0,0 +1,31 @@
+package tech.beshu.ror.integration;
+
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+
+import tech.beshu.ror.utils.containers.LdapContainer;
+
+/**
+ * This is really useful when you want to stand up a LDAP server for manual tests
+ */
+public class LDAPServer {
+  public static void main(String[] args) {
+    LdapContainer lc = LdapContainer.create("/ldap_integration_group_headers/ldap.ldif");
+    lc.start();
+    System.out.println(lc.getLdapHost() + " " + lc.getLdapPort());
+  }
+}
