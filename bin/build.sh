6f41fed05ddbd2afed6ee1b49fb42aead615642f
Simone Scarduzio <scarduzio@gmail.com>
2018-06-18 16:05:16 +0200

    create builds for 6.3

--- bin/build.sh
+++ bin/build.sh
@@ -107,6 +107,7 @@ if [[ $TRAVIS != "true" ]] ||  [[ $ROR_TASK == "package" ]]; then
     ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.6'
     ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.7'
     ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.8'
+    ./gradlew --stacktrace es53x:ror '-PesVersion=5.6.9'
     #./gradlew --stacktrace es53x:ror '-PesVersion=5.6.9'
 
     ./gradlew --stacktrace es53x:ror '-PesVersion=5.5.0'
