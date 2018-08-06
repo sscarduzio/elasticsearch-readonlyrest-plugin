bfbbb50cc0f9573e84876b75162a1eb117844940
rbkasat <rbkasat@gmail.com>
2018-08-06 15:20:56 -0700

    cross cluser search support, copy of zk index filter branch

--- core/src/test/java/tech/beshu/ror/mocks/RequestContextMock.java
+++ core/src/test/java/tech/beshu/ror/mocks/RequestContextMock.java
@@ -21,11 +21,15 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import tech.beshu.ror.commons.domain.LoggedUser;
 import tech.beshu.ror.commons.Constants;
+import tech.beshu.ror.acl.domain.LoggedUser;
+import tech.beshu.ror.httpclient.HttpMethod;
 import tech.beshu.ror.requestcontext.RequestContext;
 
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doAnswer;
@@ -62,5 +66,115 @@ public class RequestContextMock {
     }).when(mock).setResponseHeader(any(String.class), any(String.class));
 
     return mock;
+  }
+
+
+  public static RequestContext mkSearchRequest(Set<String> initialIndices, Set<String> indicesInCluster, Set<String> expandedIndices) {
+    return new RequestContext("search_test", new MockedESContext()) {
+      @Override
+      public Set<String> getExpandedIndices(Set<String> i) {
+        return expandedIndices;
+      }
+
+      @Override
+      public Set<String> getAllIndicesAndAliases() {
+        return indicesInCluster;
+      }
+
+      @Override
+      protected Boolean extractDoesInvolveIndices() {
+        return true;
+      }
+
+      @Override
+      protected Set<String> extractIndices() {
+        return initialIndices;
+      }
+
+      @Override
+      protected Boolean extractIsReadRequest() {
+        return true;
+      }
+
+      @Override
+      protected Boolean extractIsCompositeRequest() {
+        return false;
+      }
+
+      @Override
+      protected void writeIndices(Set<String> indices) {
+
+      }
+
+      @Override
+      protected void commitResponseHeaders(Map<String, String> hmap) {
+
+      }
+
+      @Override
+      public String getAction() {
+        return "indices:data/read/search";
+      }
+
+      @Override
+      public String getId() {
+        return "test_request_" + UUID.randomUUID().toString();
+      }
+
+      @Override
+      public String getContent() {
+        return null;
+      }
+
+      @Override
+      public Integer getContentLength() {
+        return null;
+      }
+
+      @Override
+      public HttpMethod getMethod() {
+        return HttpMethod.GET;
+      }
+
+      @Override
+      public String getUri() {
+        return null;
+      }
+
+      @Override
+      public String getType() {
+        return "test-SearchRequest";
+      }
+
+      @Override
+      public Long getTaskId() {
+        return 1l;
+      }
+
+      @Override
+      public String getRemoteAddress() {
+        return null;
+      }
+
+      @Override
+      protected Map<String, String> extractRequestHeaders() {
+        return Maps.newHashMap();
+      }
+
+      @Override
+      public String getHistoryString() {
+        return "no history, this is a test search request";
+      }
+
+      @Override
+      public String getMethodString() {
+        return "GET";
+      }
+
+      @Override
+      public Optional<String> getLoggedInUserName() {
+        return Optional.empty();
+      }
+    };
   }
 }
