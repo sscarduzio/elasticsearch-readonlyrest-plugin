0e472463d3db1920b5bab9f3a53660c796c8a27c
Simone Scarduzio <scarduzio@gmail.com>
2018-11-12 00:57:48 +0100

    preparation for LDAP HA

--- core/src/main/java/tech/beshu/ror/acl/definitions/ldaps/unboundid/UnboundidConnection.java
+++ core/src/main/java/tech/beshu/ror/acl/definitions/ldaps/unboundid/UnboundidConnection.java
@@ -14,6 +14,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
  */
+
 package tech.beshu.ror.acl.definitions.ldaps.unboundid;
 
 import com.unboundid.ldap.sdk.BindResult;
@@ -22,6 +23,7 @@ import com.unboundid.ldap.sdk.LDAPConnectionOptions;
 import com.unboundid.ldap.sdk.LDAPConnectionPool;
 import com.unboundid.ldap.sdk.LDAPException;
 import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.ldap.sdk.SingleServerSet;
 import com.unboundid.util.ssl.SSLUtil;
 import com.unboundid.util.ssl.TrustAllTrustManager;
 import tech.beshu.ror.acl.definitions.ldaps.LdapClientException;
@@ -37,7 +39,7 @@ public class UnboundidConnection {
   private LDAPConnectionPool connectionPool;
 
   public UnboundidConnection(ConnectionConfig connectionConfig,
-                             Optional<SearchingUserConfig> searchingUserConfig) {
+      Optional<SearchingUserConfig> searchingUserConfig) {
     connect(connectionConfig, searchingUserConfig);
   }
 
@@ -51,22 +53,22 @@ public class UnboundidConnection {
         if (connectionConfig.isSslEnabled()) {
           SSLUtil sslUtil = connectionConfig.isTrustAllCerts() ? new SSLUtil(new TrustAllTrustManager()) : new SSLUtil();
           SSLSocketFactory sslSocketFactory = sslUtil.createSSLSocketFactory();
-          connection = new LDAPConnection(sslSocketFactory, options);
+          connection = new SingleServerSet(connectionConfig.getHost(), connectionConfig.getPort(), sslSocketFactory, options).getConnection();
         }
         else {
-          connection = new LDAPConnection(options);
+          connection = new SingleServerSet(connectionConfig.getHost(), connectionConfig.getPort(), options).getConnection();
         }
         connection.connect(
-          connectionConfig.getHost(),
-          connectionConfig.getPort(),
-          (int) connectionConfig.getConnectionTimeout().toMillis()
+            connectionConfig.getHost(),
+            connectionConfig.getPort(),
+            (int) connectionConfig.getConnectionTimeout().toMillis()
         );
         searchingUserConfig.ifPresent(config -> {
           try {
             BindResult result = connection.bind(config.getDn(), config.getPassword());
             if (!ResultCode.SUCCESS.equals(result.getResultCode())) {
               throw new LdapClientException.InitializationException("LDAP binding problem - returned [" +
-                                                                      result.getResultString() + "]");
+                  result.getResultString() + "]");
             }
           } catch (LDAPException e) {
             throw new LdapClientException.InitializationException("LDAP binding problem", e);
