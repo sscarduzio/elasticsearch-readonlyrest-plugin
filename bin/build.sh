a83bdbe1735e651c491e98cb2995e83d97748544
Simone Scarduzio <scarduzio@gmail.com>
2019-03-18 13:15:15 +0100

    6.6.2 compatibility

--- bin/build.sh
+++ bin/build.sh
@@ -71,6 +71,7 @@ if [[ $TRAVIS != "true" ]] ||  [[ $ROR_TASK == "package" ]]; then
     # es66
     ./gradlew --stacktrace es66x:ror '-PesVersion=6.6.0'
     ./gradlew --stacktrace es66x:ror '-PesVersion=6.6.1'
+    ./gradlew --stacktrace es66x:ror '-PesVersion=6.6.2'
 
     # es65
     ./gradlew --stacktrace es63x-experimental:ror '-PesVersion=6.5.0'
