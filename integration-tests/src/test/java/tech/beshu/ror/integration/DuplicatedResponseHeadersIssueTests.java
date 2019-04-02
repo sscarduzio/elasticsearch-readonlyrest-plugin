b81c2e05f53007aa393171153192fef3ee48279f
Mateusz Kołodziejczyk <mkl@touk.pl>
2019-04-02 21:35:17 +0200

    added: test for bug reproduction

---
+++ integration-tests/src/test/java/tech/beshu/ror/integration/DuplicatedResponseHeadersIssueTests.java
@@ -0,0 +1,128 @@
+package tech.beshu.ror.integration;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.ClassRule;
+import org.junit.Test;
+import tech.beshu.ror.utils.containers.ESWithReadonlyRestContainer;
+import tech.beshu.ror.utils.containers.ESWithReadonlyRestContainerUtils;
+import tech.beshu.ror.utils.containers.MultiContainer;
+import tech.beshu.ror.utils.containers.MultiContainerDependent;
+import tech.beshu.ror.utils.containers.WireMockContainer;
+import tech.beshu.ror.utils.gradle.RorPluginGradleProject;
+import tech.beshu.ror.utils.httpclient.RestClient;
+import tech.beshu.ror.utils.integration.ElasticsearchTweetsInitializer;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class DuplicatedResponseHeadersIssueTests {
+
+  @ClassRule
+  public static MultiContainerDependent<ESWithReadonlyRestContainer> multiContainerDependent =
+      ESWithReadonlyRestContainerUtils.create(
+          RorPluginGradleProject.fromSystemProperty(),
+          new MultiContainer.Builder()
+              .add("EXT1", () -> WireMockContainer.create(
+                  "/duplicated_response_headers_issue/auth.json",
+                  "/duplicated_response_headers_issue/brian_groups.json",
+                  "/duplicated_response_headers_issue/freddie_groups.json"
+              ))
+              .build(), "/duplicated_response_headers_issue/elasticsearch.yml",
+          new ElasticsearchTweetsInitializer()
+      );
+
+  @Test
+  public void everySearchCallForEachUserShouldReturnTheSameResult() throws Exception {
+    RestClient freddieHttpClient = multiContainerDependent.getContainer().getBasicAuthClient("freddie", "freddie");
+    RestClient brianHttpClient = multiContainerDependent.getContainer().getBasicAuthClient("brian", "brian");
+
+    SearchResult f1 = searchCall(freddieHttpClient);
+    SearchResult f2 = searchCall(freddieHttpClient);
+    SearchResult b1 = searchCall(brianHttpClient);
+    SearchResult b2 = searchCall(brianHttpClient);
+    SearchResult b3 = searchCall(brianHttpClient);
+    SearchResult f3 = searchCall(freddieHttpClient);
+    SearchResult b4 = searchCall(brianHttpClient);
+    SearchResult b5 = searchCall(brianHttpClient);
+    SearchResult b6 = searchCall(brianHttpClient);
+    SearchResult b7 = searchCall(brianHttpClient);
+    SearchResult b8 = searchCall(brianHttpClient);
+
+    assertEquals(f2, f1);
+    assertEquals(f3, f1);
+    assertEquals(b2, b1);
+    assertEquals(b3, b1);
+    assertEquals(b4, b1);
+    assertEquals(b5, b1);
+    assertEquals(b6, b1);
+    assertEquals(b7, b1);
+    assertEquals(b8, b1);
+  }
+
+  private SearchResult searchCall(RestClient client) throws Exception {
+    HttpGet request = new HttpGet(client.from("/neg*/_search"));
+    try (CloseableHttpResponse response = client.execute(request)) {
+      return new SearchResult(
+          response.getStatusLine().getStatusCode(),
+          Lists.newArrayList(response.getAllHeaders())
+          );
+    }
+  }
+
+  private static class SearchResult {
+
+    final Integer responseCode;
+    final List<SimpleHeader> headers;
+
+    SearchResult(Integer responseCode, List<Header> headers) {
+      this.responseCode = responseCode;
+      this.headers = headers.stream().map(h -> new SimpleHeader(h.getName(), h.getValue())).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      final SearchResult other = (SearchResult) obj;
+      return Objects.equals(this.responseCode, other.responseCode)
+          && Iterables.elementsEqual(this.headers, other.headers);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.responseCode, this.headers);
+    }
+  }
+
+  private static class SimpleHeader {
+    final String name;
+    final String value;
+
+    SimpleHeader(String name, String value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      final SimpleHeader other = (SimpleHeader) obj;
+      return Objects.equals(this.name, other.name)
+          && Objects.equals(this.value, other.value);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.name, this.value);
+    }
+
+  }
+}
