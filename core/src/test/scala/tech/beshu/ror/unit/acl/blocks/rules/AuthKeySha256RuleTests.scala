a316c8996d76ca85782ec16c472c85fc0717b8e3
Simone Scarduzio <scarduzio@gmail.com>
2019-06-12 18:13:09 +0200

    add the tests also for other sha*

--- core/src/test/scala/tech/beshu/ror/unit/acl/blocks/rules/AuthKeySha256RuleTests.scala
+++ core/src/test/scala/tech/beshu/ror/unit/acl/blocks/rules/AuthKeySha256RuleTests.scala
@@ -24,3 +24,9 @@ class AuthKeySha256RuleTests extends BasicAuthenticationTestTemplate {
   override protected def ruleName: String = classOf[AuthKeySha256Rule].getSimpleName
   override protected val rule = new AuthKeySha256Rule(BasicAuthenticationRule.Settings(Secret("280ac6f756a64a80143447c980289e7e4c6918b92588c8095c7c3f049a13fbf9")))
 }
+
+class AuthKeySha256RuleAltSyntaxTests extends BasicAuthenticationTestTemplate {
+
+  override protected def ruleName: String = classOf[AuthKeySha256Rule].getSimpleName
+  override protected val rule = new AuthKeySha256Rule(BasicAuthenticationRule.Settings(Secret("logstash:76cd2c0d589e224531fc6af2c5850e3c9b2aca6902d813ce598833c7c1b28bee")))
+}
