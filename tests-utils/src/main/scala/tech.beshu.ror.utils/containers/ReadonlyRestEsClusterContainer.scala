455fd8601ed7bada0fd96a466f1a556bded73865
pondzix <pondzix@gmail.com>
2019-10-12 21:57:09 +0200

    removed: old integration test

--- tests-utils/src/main/scala/tech.beshu.ror.utils/containers/ReadonlyRestEsClusterContainer.scala
+++ tests-utils/src/main/scala/tech.beshu.ror.utils/containers/ReadonlyRestEsClusterContainer.scala
@@ -38,54 +38,50 @@ object ReadonlyRestEsCluster {
 
   def createLocalClusterContainer(name: String,
                                   rorConfigFileName: String,
-                                  numberOfInstances: Int = 2,
-                                  nodeDataInitializer: ElasticsearchNodeDataInitializer = NoOpElasticsearchNodeDataInitializer,
-                                  clusterInitializer: ReadonlyRestEsClusterInitializer = NoOpReadonlyRestEsClusterInitializer,
-                                  dependentServicesContainers: List[DependencyDef] = Nil,
-                                  configHotReloadingEnabled: Boolean = true,
-                                  internodeSslEnabled: Boolean = false): ReadonlyRestEsClusterContainer =
-    createLocalClusterContainer(
-      name,
-      ContainerUtils.getResourceFile(rorConfigFileName),
-      numberOfInstances,
-      nodeDataInitializer,
-      clusterInitializer,
-      Nil,
-      configHotReloadingEnabled,
-      internodeSslEnabled
-    )
+                                  clusterSettings: AdditionalClusterSettings = AdditionalClusterSettings()): ReadonlyRestEsClusterContainer = {
+    if (clusterSettings.numberOfInstances < 1) throw new IllegalArgumentException("ES Cluster should have at least one instance")
 
-  def createLocalClusterContainer(name: String,
-                                  rorConfigFile: File,
-                                  numberOfInstances: Int,
-                                  nodeDataInitializer: ElasticsearchNodeDataInitializer,
-                                  clusterInitializer: ReadonlyRestEsClusterInitializer,
-                                  dependentServicesContainers: List[DependencyDef],
-                                  configHotReloadingEnabled: Boolean,
-                                  internodeSslEnabled: Boolean): ReadonlyRestEsClusterContainer = {
-    if (numberOfInstances < 1) throw new IllegalArgumentException("ES Cluster should have at least one instance")
     val project = RorPluginGradleProject.fromSystemProperty
     val rorPluginFile: File = project.assemble.getOrElse(throw new ContainerCreationException("Plugin file assembly failed"))
     val esVersion = project.getESVersion
+    val rorConfigFile = ContainerUtils.getResourceFile(rorConfigFileName)
+    val nodeNames = NonEmptyList.fromListUnsafe(Seq.iterate(1, clusterSettings.numberOfInstances)(_ + 1).toList.map(idx => s"${name}_$idx"))
 
-    val nodeNames = NonEmptyList.fromListUnsafe(Seq.iterate(1, numberOfInstances)(_ + 1).toList.map(idx => s"${name}_$idx"))
     new ReadonlyRestEsClusterContainer(
       nodeNames.map { name =>
-        Task(ReadonlyRestEsContainer.create(
-          ReadonlyRestEsContainer.Config(name, nodeNames, esVersion, rorPluginFile, rorConfigFile, configHotReloadingEnabled, internodeSslEnabled),
-          nodeDataInitializer
-        ))
+        val containerConfig = ReadonlyRestEsContainer.Config(
+          nodeName = name,
+          nodes = nodeNames,
+          esVersion = esVersion,
+          rorPluginFile = rorPluginFile,
+          rorConfigFile = rorConfigFile,
+          configHotReloadingEnabled = clusterSettings.configHotReloadingEnabled,
+          internodeSslEnabled = clusterSettings.internodeSslEnabled)
+        Task(ReadonlyRestEsContainer.create(containerConfig, clusterSettings.nodeDataInitializer))
       },
-      dependentServicesContainers,
-      clusterInitializer
+      clusterSettings.dependentServicesContainers,
+      clusterSettings.clusterInitializer
     )
   }
 
   def createRemoteClustersContainer(localClusters: NonEmptyList[LocalClusterDef],
                                     remoteClustersInitializer: RemoteClustersInitializer): ReadonlyRestEsRemoteClustersContainer = {
-    val startedClusters = localClusters.map(l => createLocalClusterContainer(l.name, l.rorConfigFileName, numberOfInstances = 1, l.nodeDataInitializer))
+    val startedClusters = localClusters
+      .map { cluster =>
+        createLocalClusterContainer(
+          cluster.name,
+          cluster.rorConfigFileName,
+          AdditionalClusterSettings(nodeDataInitializer = cluster.nodeDataInitializer))
+      }
     new ReadonlyRestEsRemoteClustersContainer(startedClusters, remoteClustersInitializer)
   }
+
+  final case class AdditionalClusterSettings(numberOfInstances: Int = 1,
+                                             nodeDataInitializer: ElasticsearchNodeDataInitializer = NoOpElasticsearchNodeDataInitializer,
+                                             clusterInitializer: ReadonlyRestEsClusterInitializer = NoOpReadonlyRestEsClusterInitializer,
+                                             dependentServicesContainers: List[DependencyDef] = Nil,
+                                             configHotReloadingEnabled: Boolean = true,
+                                             internodeSslEnabled: Boolean = false)
 }
 
 final case class LocalClusterDef(name: String, rorConfigFileName: String, nodeDataInitializer: ElasticsearchNodeDataInitializer)
