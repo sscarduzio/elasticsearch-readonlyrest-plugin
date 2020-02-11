2c7fc5018f1964526042f3771f323919877947cb
wdk <clutroth@users.noreply.github.com>
2020-02-11 19:55:09 +0100

    [RORDEV-195] cutoff ipv6 zone id (#561)
    Enhancement (ES) literal ipv6 with zone id is acceptable network address.

--- core/src/main/scala/tech/beshu/ror/accesscontrol/domain.scala
+++ core/src/main/scala/tech/beshu/ror/accesscontrol/domain.scala
@@ -19,7 +19,7 @@ package tech.beshu.ror.accesscontrol
 import java.nio.charset.StandardCharsets.UTF_8
 import java.util.{Base64, Locale}
 
-import cats.Eq
+import cats.{Eq, Functor}
 import cats.data.NonEmptyList
 import cats.implicits._
 import com.comcast.ip4s.interop.cats.HostnameResolver
@@ -138,14 +138,35 @@ object domain {
     final case class Name(value: Hostname) extends Address
 
     def from(value: String): Option[Address] = {
-      Cidr.fromString(value).map(Ip.apply) orElse
-        IpAddress(value).map(ip => Ip(Cidr(ip, 32))) orElse
-        Hostname(value).map(Name.apply)
+      parseCidr(value) orElse
+        parseIpAddress(value) orElse
+        parseHostname(value)
     }
 
     // fixme: (improvements) blocking resolving (shift to another EC)
     def resolve(hostname: Name): Task[Option[NonEmptyList[Ip]]] = {
       HostnameResolver.resolveAll[Task](hostname.value).map(_.map(_.map(ip => Ip(Cidr(ip, 32)))))
+    }
+  }
+
+  private def parseCidr(value: String) =
+    Cidr.fromString(value).map(Address.Ip.apply)
+
+  private def parseHostname(value: String) =
+    Hostname(value).map(Address.Name.apply)
+
+  private def parseIpAddress(value: String) =
+    (cutOffZoneIndex _ andThen IpAddress.apply andThen (_.map(createAddressIp))) (value)
+
+  private def createAddressIp(ip: IpAddress) =
+    Address.Ip(Cidr(ip, 32))
+
+  private val ipv6WithLiteralScope = raw"""(?i)^(fe80:[a-z0-9:]+)%.*$$""".r
+
+  private def cutOffZoneIndex(value: String): String = { //https://en.wikipedia.org/wiki/IPv6_address#Scoped_literal_IPv6_addresses
+    value match {
+      case ipv6WithLiteralScope(ipv6) => ipv6
+      case noLiteralIp => noLiteralIp
     }
   }
 
