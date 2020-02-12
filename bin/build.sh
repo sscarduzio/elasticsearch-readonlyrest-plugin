c00c8fc57ab98688f98b95f5983916200e62aaca
wdk <clutroth@users.noreply.github.com>
2020-02-12 12:35:58 +0100

    es 7.6.0 support (#563)
    * es 7.6.0 support

--- bin/build.sh
+++ bin/build.sh
@@ -163,6 +163,7 @@ if [[ $TRAVIS != "true" ]] ||  [[ $ROR_TASK == "package_es7xx" ]]; then
     ./gradlew --stacktrace es74x:ror '-PesVersion=7.5.0'
     ./gradlew --stacktrace es74x:ror '-PesVersion=7.5.1'
     ./gradlew --stacktrace es74x:ror '-PesVersion=7.5.2'
+    ./gradlew --stacktrace es74x:ror '-PesVersion=7.6.0'
 
     #es73
     ./gradlew --stacktrace es73x:ror '-PesVersion=7.3.0'
