46eca17e2e898f2d5c515b7e98df733723311184
Mateusz Kołodziejczyk <mkl@touk.pl>
2020-04-28 16:53:50 +0200

    clean up

--- core/src/main/scala/tech/beshu/ror/accesscontrol/blocks/BlockContext.scala
+++ core/src/main/scala/tech/beshu/ror/accesscontrol/blocks/BlockContext.scala
@@ -190,7 +190,7 @@ object BlockContext {
     }
 
     def nonExistingIndicesFromInitialIndices(): Set[IndexName] = {
-      blockContext.indices.map(i => IndexName.randomNonexistentIndex2(i.value.value))
+      blockContext.indices.map(i => IndexName.randomNonexistentIndex(i.value.value))
     }
   }
 }
--- core/src/main/scala/tech/beshu/ror/accesscontrol/utils/IndicesListOps.scala
+++ core/src/main/scala/tech/beshu/ror/accesscontrol/utils/IndicesListOps.scala
@@ -9,11 +9,11 @@ class IndicesListOps(val indices: List[IndexName]) extends AnyVal {
   def randomNonexistentIndex(): IndexName = {
     val foundIndex = indices.find(_.hasWildcard) orElse indices.headOption
     foundIndex match {
-      case Some(indexName) if indexName.isClusterIndex => IndexName.randomNonexistentIndex2(
+      case Some(indexName) if indexName.isClusterIndex => IndexName.randomNonexistentIndex(
         indexName.value.value.replace(":", "_") // we don't want to call remote cluster
       )
-      case Some(indexName) => IndexName.randomNonexistentIndex2(indexName.value.value)
-      case None => IndexName.randomNonexistentIndex2()
+      case Some(indexName) => IndexName.randomNonexistentIndex(indexName.value.value)
+      case None => IndexName.randomNonexistentIndex()
     }
   }
 
