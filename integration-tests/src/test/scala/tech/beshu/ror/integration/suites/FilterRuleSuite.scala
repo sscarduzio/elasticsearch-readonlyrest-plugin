3be70dd74605b0a3971cd20d7daa959ce21776b2
pondzix <pondzix@gmail.com>
2020-05-20 18:08:52 +0200

    added: compilation fix + prefix filter query test

--- integration-tests/src/test/scala/tech/beshu/ror/integration/suites/FilterRuleSuite.scala
+++ integration-tests/src/test/scala/tech/beshu/ror/integration/suites/FilterRuleSuite.scala
@@ -54,7 +54,7 @@ trait FilterRuleSuite
             result.responseCode shouldBe 200
             result.searchHits.size shouldBe 1
 
-            result.head shouldBe ujson.read("""{"db_name":"db_user1", "code": 1}""")
+            result.head shouldBe ujson.read("""{"db_name":"db_user1", "code": 1, "status": "ok"}""")
           }
         }
         "there is no query in request body" in {
@@ -66,8 +66,8 @@ trait FilterRuleSuite
             result.searchHits.size shouldBe 2
             result.docIds should contain allOf("1", "2")
 
-            result.id("1") shouldBe ujson.read("""{"db_name":"db_user1", "code": 1}""")
-            result.id("2") shouldBe ujson.read("""{"db_name":"db_user1", "code": 2}""")
+            result.id("1") shouldBe ujson.read("""{"db_name":"db_user1", "code": 1, "status": "ok"}""")
+            result.id("2") shouldBe ujson.read("""{"db_name":"db_user1", "code": 2, "status": "ok"}""")
           }
         }
         "wildcard in filter query is used" in {
@@ -78,7 +78,18 @@ trait FilterRuleSuite
             result.responseCode shouldBe 200
             result.searchHits.size shouldBe 1
 
-            result.head shouldBe ujson.read("""{"db_name":"db_user2", "code": 1}""")
+            result.head shouldBe ujson.read("""{"db_name":"db_user2", "code": 1, "status": "ok"}""")
+          }
+        }
+        "prefix in filter query is used" in {
+          retry(times = 3) {
+            val searchManager = new SearchManager(basicAuthClient("user3", "pass"))
+            val result = searchManager.search("/test1_index/_search")
+
+            result.responseCode shouldBe 200
+            result.searchHits.size shouldBe 1
+
+            result.head shouldBe ujson.read("""{"db_name":"db_user3", "code": 2, "status": "wrong"}""")
           }
         }
       }
@@ -129,7 +140,7 @@ trait FilterRuleSuite
       }
       "return error" when {
         "filter query is malformed" in {
-          val searchManager = new SearchManager(basicAuthClient("user3", "pass"))
+          val searchManager = new SearchManager(basicAuthClient("user4", "pass"))
           val result = searchManager.search("/test1_index/_search", """{ "query": { "term": { "code": 1 }}}""")
 
           if (Version.greaterOrEqualThan(targetEs.esVersion, 7, 6, 0)) {
@@ -148,9 +159,9 @@ object FilterRuleSuite {
   private def nodeDataInitializer(): ElasticsearchNodeDataInitializer = (esVersion, adminRestClient: RestClient) => {
     val documentManager = new DocumentManager(adminRestClient, esVersion)
 
-    documentManager.createDoc("test1_index", 1, ujson.read("""{"db_name":"db_user1", "code": 1}"""))
-    documentManager.createDoc("test1_index", 2, ujson.read("""{"db_name":"db_user1", "code": 2}"""))
-    documentManager.createDoc("test1_index", 3, ujson.read("""{"db_name":"db_user2", "code": 1}"""))
-    documentManager.createDoc("test1_index", 4, ujson.read("""{"db_name":"db_user3", "code": 2}"""))
+    documentManager.createDoc("test1_index", 1, ujson.read("""{"db_name":"db_user1", "code": 1, "status": "ok"}"""))
+    documentManager.createDoc("test1_index", 2, ujson.read("""{"db_name":"db_user1", "code": 2, "status": "ok"}"""))
+    documentManager.createDoc("test1_index", 3, ujson.read("""{"db_name":"db_user2", "code": 1, "status": "ok"}"""))
+    documentManager.createDoc("test1_index", 4, ujson.read("""{"db_name":"db_user3", "code": 2, "status": "wrong"}"""))
   }
 }
