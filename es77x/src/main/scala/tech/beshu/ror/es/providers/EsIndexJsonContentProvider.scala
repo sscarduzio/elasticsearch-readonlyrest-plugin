cabf2bb2d1ee3b971a4c7930f39b288436840095
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2020-05-22 21:59:05 +0200

    [RORDEV-261] error logging in index content provider (#596)
    error logging in index content provider

--- es77x/src/main/scala/tech/beshu/ror/es/providers/EsIndexJsonContentProvider.scala
+++ es77x/src/main/scala/tech/beshu/ror/es/providers/EsIndexJsonContentProvider.scala
@@ -18,67 +18,83 @@ package tech.beshu.ror.es.providers
 
 import java.util
 
+import cats.implicits._
 import com.google.common.collect.Maps
 import monix.eval.Task
-import monix.execution.CancelablePromise
+import org.apache.logging.log4j.scala.Logging
 import org.elasticsearch.ResourceNotFoundException
-import org.elasticsearch.action.ActionListener
-import org.elasticsearch.action.bulk.BulkResponse
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 import org.elasticsearch.client.node.NodeClient
-import org.elasticsearch.common.inject.Inject
+import org.elasticsearch.common.inject.{Inject, Singleton}
 import org.elasticsearch.common.xcontent.XContentType
-import tech.beshu.ror.accesscontrol.domain.IndexName
+import tech.beshu.ror.accesscontrol.domain
+import tech.beshu.ror.accesscontrol.show.logs._
 import tech.beshu.ror.boot.Ror
 import tech.beshu.ror.es.IndexJsonContentManager
 import tech.beshu.ror.es.IndexJsonContentManager._
 
-import scala.concurrent.Promise
-import scala.util.{Failure, Success, Try}
-
+@Singleton
 class EsIndexJsonContentProvider(client: NodeClient,
                                  ignore: Unit) // hack!
-  extends IndexJsonContentManager {
+  extends IndexJsonContentManager
+    with Logging {
 
   @Inject
   def this(client: NodeClient) {
     this(client, ())
   }
 
-  override def sourceOf(index: IndexName,
+  override def sourceOf(index: domain.IndexName,
                         `type`: String,
-                        id: String): Task[Either[ReadError, util.Map[String, _]]] = {
-    Task
-      .eval(getSourceCall(index.value.value, `type`, id))
-      .map {
-        case Success(value) => Right(value)
-        case Failure(_: ResourceNotFoundException) => Left(ContentNotFound: ReadError)
-        case Failure(_) => Left(CannotReachContentSource: ReadError)
+                        id: String): Task[Either[ReadError, util.Map[String, AnyRef]]] = Task(
+    client
+      .get(client.prepareGet(index.value.value, `type`, id).request())
+      .actionGet()
+  )
+    .map { response =>
+      Option(response.getSourceAsMap) match {
+        case Some(map) =>
+          Right(map)
+        case None =>
+          logger.warn(s"Document ${index.show}/${`type`}/$id _source is not available. Assuming it's empty")
+          Right(Maps.newHashMap[String, AnyRef]())
       }
-      .executeOn(Ror.blockingScheduler)
-  }
+    }
+    .executeOn(Ror.blockingScheduler)
+    .onErrorRecover {
+      case _: ResourceNotFoundException => Left(ContentNotFound)
+      case ex =>
+        logger.error(s"Cannot get source of document ${index.show}/${`type`}/$id", ex)
+        Left(CannotReachContentSource)
+    }
 
-  override def saveContent(index: IndexName,
+  override def saveContent(index: domain.IndexName,
                            `type`: String,
                            id: String,
-                           content: util.Map[String, String]): Task[Either[WriteError, Unit]] = {
-    val promise = CancelablePromise[Either[WriteError, Unit]]()
+                           content: util.Map[String, String]): Task[Either[WriteError, Unit]] = Task(
     client
-      .prepareBulk
-      .add(client.prepareIndex(index.value.value, `type`, id).setSource(content, XContentType.JSON).request)
-      .execute(new PromiseActionListenerAdapter(promise))
-    Task.fromCancelablePromise(promise)
-  }
-
-  private def getSourceCall(index: String, `type`: String, id: String): Try[util.Map[String, _]] = Try {
-    val response = client.get(client.prepareGet(index, `type`, id).request).actionGet
-    Option(response.getSourceAsMap).getOrElse(Maps.newHashMap())
-  }
-
-  private class PromiseActionListenerAdapter(promise: Promise[Either[WriteError, Unit]])
-    extends ActionListener[BulkResponse] {
-
-    override def onResponse(response: BulkResponse): Unit = promise.success(Right(()))
-
-    override def onFailure(e: Exception): Unit = promise.success(Left(CannotWriteToIndex(e)))
-  }
+      .index(
+        client
+          .prepareIndex(index.value.value, `type`, id)
+          .setSource(content, XContentType.JSON)
+          .setRefreshPolicy(RefreshPolicy.WAIT_UNTIL)
+          .request()
+      )
+      .actionGet()
+  )
+    .map { response =>
+      response.status().getStatus match {
+        case status if status / 100 == 2 =>
+          Right(())
+        case status =>
+          logger.error(s"Cannot write to document ${index.show}/${`type`}/$id. Unexpected response: HTTP $status, response: ${response.toString}")
+          Left(CannotWriteToIndex)
+      }
+    }
+    .executeOn(Ror.blockingScheduler)
+    .onErrorRecover {
+      case ex =>
+        logger.error(s"Cannot write to document ${index.show}/${`type`}/$id", ex)
+        Left(CannotWriteToIndex)
+    }
 }
