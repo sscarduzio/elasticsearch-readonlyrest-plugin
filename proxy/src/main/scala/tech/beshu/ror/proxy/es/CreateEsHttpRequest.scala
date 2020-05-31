f921cc4d13f8049efd4222ace89aa6e1ab87039e
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2020-05-31 20:18:57 +0200

    added: GET / response should be the same as GET /?pretty

--- proxy/src/main/scala/tech/beshu/ror/proxy/es/CreateEsHttpRequest.scala
+++ proxy/src/main/scala/tech/beshu/ror/proxy/es/CreateEsHttpRequest.scala
@@ -6,11 +6,14 @@ package tech.beshu.ror.proxy.es
 import java.util
 
 import com.twitter.finagle.http.{Method, Request, Version}
+import io.lemonlabs.uri.config.UriConfig
+import io.lemonlabs.uri.{AbsolutePath, RelativeUrl, UrlPath}
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.netty.handler.codec.http.cookie.{ServerCookieDecoder, ServerCookieEncoder}
 import org.elasticsearch.common.bytes.{BytesArray, BytesReference}
-import org.elasticsearch.rest.{RestRequest, RestStatus}
 import org.elasticsearch.http.{HttpRequest => EsHttpRequest, HttpResponse => EsHttpResponse}
+import org.elasticsearch.rest.{RestRequest, RestStatus}
+
 import scala.collection.JavaConverters._
 
 object CreateEsHttpRequest {
@@ -26,6 +29,18 @@ object CreateEsHttpRequest {
 
   private def from(request: Request,
                    headers: Map[String, List[String]]): EsHttpRequest = new EsHttpRequest {
+
+    private lazy val requestUri = {
+      implicit val uriConfig: UriConfig = UriConfig.default
+      RelativeUrl.parseOption(request.uri) match {
+        case Some(url) if slashOrEmpty(url.path) =>
+          // GET / response is rendered as GET /?pretty - proxy wants to reproduce this strange ES behaviour
+          url.copy(query = url.query.addParam("pretty")).toString()
+        case _ =>
+          request.uri
+      }
+    }
+
     override def method(): RestRequest.Method = request.method match {
       case Method.Get => RestRequest.Method.GET
       case Method.Post => RestRequest.Method.POST
@@ -39,7 +54,7 @@ object CreateEsHttpRequest {
       case unsupported => throw new IllegalArgumentException(s"Unsupported http method: $unsupported")
     }
 
-    override def uri(): String = request.uri
+    override def uri(): String = requestUri
 
     override def content(): BytesReference = new BytesArray(request.contentString.getBytes)
 
@@ -54,7 +69,7 @@ object CreateEsHttpRequest {
           ServerCookieDecoder.STRICT.decode(cookieStr).asScala
         }
         .toList
-      if(cookies.nonEmpty) ServerCookieEncoder.STRICT.encode(cookies.asJava)
+      if (cookies.nonEmpty) ServerCookieEncoder.STRICT.encode(cookies.asJava)
       else List.empty.asJava
     }
 
@@ -73,11 +88,14 @@ object CreateEsHttpRequest {
     override def createResponse(status: RestStatus, content: BytesReference): EsHttpResponse =
       new EsHttpResponse {
         override def addHeader(name: String, value: String): Unit = ()
+
         override def containsHeader(name: String): Boolean = false
       }
 
     override def release(): Unit = ()
 
     override def releaseAndCopy(): EsHttpRequest = from(request, headers)
+
+    private def slashOrEmpty(urlPath: UrlPath) = urlPath == UrlPath.slash || urlPath == AbsolutePath(Vector(""))
   }
 }
