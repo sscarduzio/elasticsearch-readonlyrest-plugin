51b41c7ebcf1308e58c0bb625cf2136342146767
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2020-06-11 18:14:51 +0200

    [RORDEV-268] ROR proxy - kibana with xpack support (#602)
    ROR proxy - kibana with xpack support

---
+++ proxy/src/main/scala/tech/beshu/ror/proxy/es/genericaction/GenericRequest.scala
@@ -0,0 +1,43 @@
+/*
+ *     Beshu Limited all rights reserved
+ */
+package tech.beshu.ror.proxy.es.genericaction
+
+import org.elasticsearch.action.{ActionRequest, ActionRequestValidationException}
+import org.elasticsearch.rest.RestRequest
+
+class GenericRequest(val rest: RestRequest) extends ActionRequest {
+  override def validate(): ActionRequestValidationException = null
+}
+
+object GenericRequest {
+
+  def from(rest: RestRequest): Option[GenericRequest] = {
+    isAllowedByPath(rest) map (_ => new GenericRequest(rest))
+  }
+
+  private def isAllowedByPath(rest: RestRequest) = {
+    allowedPathParts.find(rest.uri().contains)
+  }
+
+  private val allowedPathParts = Set(
+    "/_autoscaling",
+    "/_cat",
+    "_cat/transforms",
+    "/_cluster",
+    "/_ccr/",
+    "/_enrich",
+    "/_freeze", // todo: indices awareness
+    "/_ilm/",
+    "/_license",
+    "/_monitoring",
+    "/_nodes",
+    "/_rollup/",
+    "/_security",
+    "/_transform",
+    "/_unfreeze", // todo: indices awareness,
+    "/_watcher",
+    "/_xpack",
+  )
+
+}
