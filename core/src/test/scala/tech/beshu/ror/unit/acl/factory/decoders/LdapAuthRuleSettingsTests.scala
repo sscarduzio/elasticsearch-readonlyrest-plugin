cbd2e983a13498619370a98f4bce79206a5a6915
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2020-07-07 18:38:20 +0200

    fixed: ldap container init issue

--- core/src/test/scala/tech/beshu/ror/unit/acl/factory/decoders/LdapAuthRuleSettingsTests.scala
+++ core/src/test/scala/tech/beshu/ror/unit/acl/factory/decoders/LdapAuthRuleSettingsTests.scala
@@ -27,8 +27,8 @@ class LdapAuthRuleSettingsTests
   extends BaseRuleSettingsDecoderTest[LdapAuthRule]
     with ForAllTestContainer {
 
-  private val containerLdap1 = new LdapContainer("LDAP1", "/test_example.ldif")
-  private val containerLdap2 = new LdapContainer("LDAP2", "/test_example.ldif")
+  private val containerLdap1 = new LdapContainer("LDAP1", "test_example.ldif")
+  private val containerLdap2 = new LdapContainer("LDAP2", "test_example.ldif")
 
   override val container: MultipleContainers = MultipleContainers(containerLdap1, containerLdap2)
 
