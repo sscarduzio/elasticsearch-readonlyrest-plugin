d57727422d8c529c507de357787d6bc01e1c9d91
pondzix <pondzix@gmail.com>
2020-07-10 23:11:42 +0200

    added: variable support for field rule

--- core/src/main/scala/tech/beshu/ror/accesscontrol/factory/decoders/rules/FieldsRuleDecoder.scala
+++ core/src/main/scala/tech/beshu/ror/accesscontrol/factory/decoders/rules/FieldsRuleDecoder.scala
@@ -19,65 +19,39 @@ package tech.beshu.ror.accesscontrol.factory.decoders.rules
 import cats.data.NonEmptySet
 import cats.implicits._
 import eu.timepit.refined.types.string.NonEmptyString
-import tech.beshu.ror.accesscontrol.blocks.rules.FieldsRule
-import tech.beshu.ror.accesscontrol.factory.RawRorConfigBasedCoreFactory.AclCreationError.Reason.Message
-import tech.beshu.ror.accesscontrol.factory.RawRorConfigBasedCoreFactory.AclCreationError.RulesLevelCreationError
-import tech.beshu.ror.accesscontrol.factory.decoders.rules.RuleBaseDecoder.RuleDecoderWithoutAssociatedFields
-import tech.beshu.ror.accesscontrol.factory.decoders.rules.FieldsRuleDecoderHelper._
-import tech.beshu.ror.accesscontrol.utils.CirceOps.{DecoderHelpers, _}
 import tech.beshu.ror.Constants
+import tech.beshu.ror.accesscontrol.blocks.rules.FieldsRule
 import tech.beshu.ror.accesscontrol.blocks.rules.Rule.RuleWithVariableUsageDefinition
+import tech.beshu.ror.accesscontrol.blocks.variables.runtime.RuntimeMultiResolvableVariable
 import tech.beshu.ror.accesscontrol.domain.DocumentField
 import tech.beshu.ror.accesscontrol.domain.DocumentField.{ADocumentField, NegatedDocumentField}
+import tech.beshu.ror.accesscontrol.factory.decoders.common._
+import tech.beshu.ror.accesscontrol.factory.decoders.rules.FieldsRuleDecoderHelper.{aDocumentFieldDecoder, negatedDocumentFieldDecoder}
+import tech.beshu.ror.accesscontrol.factory.decoders.rules.RuleBaseDecoder.RuleDecoderWithoutAssociatedFields
 import tech.beshu.ror.accesscontrol.orders._
+import tech.beshu.ror.accesscontrol.utils.CirceOps.{DecoderHelpers, _}
 
 import scala.collection.JavaConverters._
-import scala.collection.SortedSet
 
 object FieldsRuleDecoder extends RuleDecoderWithoutAssociatedFields(
-  DecoderHelpers
-    .decodeStringLikeOrNonEmptySetE(toDocumentField)
-    .toSyncDecoder
-    .emapE { fields =>
-      val (negatedFields, nonNegatedFields) = fields.toList.partitionEither {
-        case d: ADocumentField => Right(d)
-        case d: NegatedDocumentField => Left(d)
-      }
-      if (negatedFields.nonEmpty && nonNegatedFields.nonEmpty) {
-        Left(RulesLevelCreationError(Message(s"fields should all be negated (i.e. '~field1') or all without negation (i.e. 'field1') Found: ${fields.map(_.value).toSortedSet.mkString(",")}")))
-      } else if (containsAlwaysAllowedFields(fields)) {
-        Left(RulesLevelCreationError(Message(s"The fields rule cannot contain always-allowed fields: ${Constants.FIELDS_ALWAYS_ALLOW.asScala.mkString(",")}")))
-      } else {
-        val settings = NonEmptySet.fromSet(SortedSet.empty[ADocumentField] ++ nonNegatedFields.toSet) match {
-          case Some(f) =>
-            FieldsRule.Settings.ofFields(f)
-          case None =>
-            val negatedFieldsNes = NonEmptySet
-              .fromSet(SortedSet.empty[NegatedDocumentField] ++ negatedFields.toSet)
-                .getOrElse(throw new IllegalStateException("Should contain all negated fields"))
-            FieldsRule.Settings.ofNegatedFields(negatedFieldsNes)
-        }
-        Right(settings)
-      }
-    }
-    .map(settings => RuleWithVariableUsageDefinition.create(new FieldsRule(settings)))
-    .decoder
+  aDocumentFieldDecoder.or(negatedDocumentFieldDecoder)
 )
 
 private object FieldsRuleDecoderHelper {
-  def toDocumentField(value: String): Either[String, DocumentField] = {
-    if (value.startsWith("~")) {
-      NonEmptyString.from(value.substring(1)) match {
-        case Right(nes) => Right(NegatedDocumentField(nes))
-        case Left(_) => Left("There was no name passed for blacklist field (~ only is forbidden)")
-      }
-    } else {
-      NonEmptyString.from(value) match {
-        case Right(nes) => Right(ADocumentField(nes))
-        case Left(_) => Left("Field cannot be empty string")
-      }
-    }
-  }
+
+  val aDocumentFieldDecoder = DecoderHelpers
+    .decodeStringLikeOrNonEmptySet[RuntimeMultiResolvableVariable[ADocumentField]]
+    .toSyncDecoder
+    .map { fields => FieldsRule.Settings.ofFields(fields) }
+    .map(settings => RuleWithVariableUsageDefinition.create(new FieldsRule(settings)))
+    .decoder
+
+  val negatedDocumentFieldDecoder = DecoderHelpers
+    .decodeStringLikeOrNonEmptySet[RuntimeMultiResolvableVariable[NegatedDocumentField]]
+    .toSyncDecoder
+    .map { fields => FieldsRule.Settings.ofNegatedFields(fields) }
+    .map(settings => RuleWithVariableUsageDefinition.create(new FieldsRule(settings)))
+    .decoder
 
   def containsAlwaysAllowedFields(fields: NonEmptySet[DocumentField]): Boolean = {
     fields.toSortedSet.map(_.value).intersect(Constants.FIELDS_ALWAYS_ALLOW.asScala.map(NonEmptyString.unsafeFrom)).nonEmpty
--- integration-tests/src/test/scala/tech/beshu/ror/integration/suites/FieldLevelSecuritySuite.scala
+++ integration-tests/src/test/scala/tech/beshu/ror/integration/suites/FieldLevelSecuritySuite.scala
@@ -46,6 +46,17 @@ trait FieldLevelSecuritySuite
 
         source should be(ujson.read("""{"dummy2":"true"}"""))
       }
+      "whitelist mode with user variable is used " in {
+        val searchManager = new SearchManager(basicAuthClient("dummy", "pass"))
+
+        val result = searchManager.search("/testfiltera/_search")
+
+        assertEquals(200, result.responseCode)
+        val searchJson = result.searchHits
+        val source = searchJson(0)("_source")
+
+        source should be(ujson.read("""{"dummy2":"true"}"""))
+      }
       "whitelist mode with wildcard is used" in {
         val searchManager = new SearchManager(basicAuthClient("user2", "pass"))
 
