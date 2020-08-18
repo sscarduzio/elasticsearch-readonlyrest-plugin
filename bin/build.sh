0a32163a7f4c2fa1f073c67a91b00bdd371e47df
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2020-08-18 20:50:02 +0200

    added ES 6.8.12 support

--- bin/build.sh
+++ bin/build.sh
@@ -156,6 +156,7 @@ if [[ $TRAVIS != "true" ]] ||  [[ $ROR_TASK == "package_es6xx" ]]; then
     ./gradlew --stacktrace es66x:ror '-PesVersion=6.8.9'
     ./gradlew --stacktrace es66x:ror '-PesVersion=6.8.10'
     ./gradlew --stacktrace es66x:ror '-PesVersion=6.8.11'
+    ./gradlew --stacktrace es66x:ror '-PesVersion=6.8.12'
 
     # es65
     ./gradlew --stacktrace es63x:ror '-PesVersion=6.5.0'
