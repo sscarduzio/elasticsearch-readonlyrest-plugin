e8c0363497a7f18cbb6074aad4556a75d938cae6
pondzix <pondzix@gmail.com>
2020-10-10 14:37:34 +0200

    added: fields rule test refactor

--- core/src/test/scala/tech/beshu/ror/mocks/MockRequestContext.scala
+++ core/src/test/scala/tech/beshu/ror/mocks/MockRequestContext.scala
@@ -20,6 +20,7 @@ import java.time.{Clock, Instant}
 
 import com.softwaremill.sttp.Method
 import squants.information.{Bytes, Information}
+import tech.beshu.ror.accesscontrol.blocks.BlockContext
 import tech.beshu.ror.accesscontrol.blocks.BlockContext.{CurrentUserMetadataRequestBlockContext, FilterableRequestBlockContext, GeneralIndexRequestBlockContext, RepositoryRequestBlockContext, SnapshotRequestBlockContext}
 import tech.beshu.ror.accesscontrol.blocks.metadata.UserMetadata
 import tech.beshu.ror.accesscontrol.domain._
@@ -40,6 +41,12 @@ object MockRequestContext {
 
   def metadata(implicit clock: Clock = Clock.systemUTC()): MockUserMetadataRequestContext =
     MockUserMetadataRequestContext(timestamp = clock.instant())
+
+  def readOnly[BC <: BlockContext](blockContextCreator: RequestContext => BC): MockSimpleRequestContext[BC] =
+    MockSimpleRequestContext(blockContextCreator, isReadOnly = true)
+
+  def notReadOnly[BC <: BlockContext](blockContextCreator: RequestContext => BC): MockSimpleRequestContext[BC] =
+    MockSimpleRequestContext(blockContextCreator, isReadOnly = false)
 }
 
 final case class MockGeneralIndexRequestContext(override val timestamp: Instant,
@@ -174,4 +181,33 @@ final case class MockUserMetadataRequestContext(override val timestamp: Instant,
   override def initialBlockContext: CurrentUserMetadataRequestBlockContext = CurrentUserMetadataRequestBlockContext(
     this, UserMetadata.empty, Set.empty
   )
+}
+
+abstract class MockSimpleRequestContext[BC <: BlockContext](override val timestamp: Instant = Instant.now(),
+                                                            override val taskId: Long = 0L,
+                                                            override val id: RequestContext.Id = RequestContext.Id("mock"),
+                                                            override val `type`: Type = Type("default-type"),
+                                                            override val action: Action = Action("default-action"),
+                                                            override val headers: Set[Header] = Set.empty,
+                                                            override val remoteAddress: Option[Address] = Address.from("localhost"),
+                                                            override val localAddress: Address = Address.from("localhost").get,
+                                                            override val method: Method = Method("GET"),
+                                                            override val uriPath: UriPath = UriPath("PATH"),
+                                                            override val contentLength: Information = Bytes(0),
+                                                            override val content: String = "",
+                                                            override val allIndicesAndAliases: Set[IndexWithAliases] = Set.empty,
+                                                            override val allTemplates: Set[Template] = Set.empty,
+                                                            override val isCompositeRequest: Boolean = false,
+                                                            override val isAllowedForDLS: Boolean = true,
+                                                            override val hasRemoteClusters: Boolean = false)
+  extends RequestContext {
+  override type BLOCK_CONTEXT = BC
+}
+
+object MockSimpleRequestContext {
+  def apply[BC <: BlockContext](blockContextCreator: RequestContext => BC,
+                                isReadOnly: Boolean): MockSimpleRequestContext[BC] = new MockSimpleRequestContext[BC] {
+    override val initialBlockContext: BC = blockContextCreator(this)
+    override val isReadOnlyRequest: Boolean = isReadOnly
+  }
 }
