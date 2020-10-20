c2b76b516d2801a65c3072c7229fda90850cb7f2
Simone Scarduzio <simone@readonlyrest.com>
2020-10-20 12:35:03 +0200

    would fetch anyway

--- ci/actionstrings/fetchIfNecessary.sh
+++ ci/actionstrings/fetchIfNecessary.sh
@@ -6,11 +6,11 @@ mkdir -p $DEST_DIR || echo "$DEST_DIR already present."
 FILENAME="$DEST_DIR/action_strings_es$ES_VERSION.txt"
 if test -f $FILENAME; then
   echo "$FILENAME exists."
-  return 0
+else
+  echo "processing ES action extractions for: $FILENAME"
+  ci/actionstrings/fetch.sh "v$ES_VERSION" > $FILENAME
+  head -3 $FILENAME
+  echo "..."
+  tail -3 $FILENAME
 fi
 
-echo "processing ES action extractions for: $FILENAME"
-ci/actionstrings/fetch.sh "v$ES_VERSION" > $FILENAME
-head -3 $FILENAME
-echo "..."
-tail -3 $FILENAME
