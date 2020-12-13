46fdd76c90beee20a413d4486705ad541aeef378
Mateusz Piękos <mpk@touk.pl>
2020-12-13 14:43:21 +0100

    Improved tests

--- tests-utils/src/main/scala/tech/beshu/ror/utils/containers/DnsServerContainer.scala
+++ tests-utils/src/main/scala/tech/beshu/ror/utils/containers/DnsServerContainer.scala
@@ -1,21 +1,26 @@
 package tech.beshu.ror.utils.containers
 
 import com.dimafeng.testcontainers.GenericContainer
+import com.github.dockerjava.api.model.{ExposedPort, InternetProtocol}
+import org.testcontainers.images.builder.ImageFromDockerfile
 
 import scala.collection.JavaConverters._
 import scala.language.{implicitConversions, postfixOps}
 
 class DnsServerContainer(srvServicePort: Int)
   extends GenericContainer(
-    dockerImage = "mycoredns:latest",
+    dockerImage = new ImageFromDockerfile()
+      .withFileFromClasspath("Dockerfile", "coredns-image/Dockerfile")
+      .withFileFromClasspath("Corefile", "coredns-image/Corefile")
+      .withFileFromClasspath("entrypoint.sh", "coredns-image/entrypoint.sh")
+      .withFileFromClasspath("conf_template", "coredns-image/conf_template"),
     env = Map("PORT" -> srvServicePort.toString)
   ) {
 
   def dnsPort: Int = {
     // This is hack to obtain mapping to UDP port as testcontainers doesn't allow explicit mapping of UDP ports
     // although it is mapping each port exposed by container(even UDP).
-    // This code gets port value of only one mapped port which should be 53/udp
-    this.containerInfo.getNetworkSettings.getPorts.getBindings.asScala.head._2.head.getHostPortSpec.toInt
+    this.containerInfo.getNetworkSettings.getPorts.getBindings.asScala.get(new ExposedPort(53, InternetProtocol.UDP)).get.head.getHostPortSpec.toInt
   }
 
   override def stop(): Unit = {
