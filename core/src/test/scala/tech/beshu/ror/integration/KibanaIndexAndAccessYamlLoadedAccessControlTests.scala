bbc814eab4f2e7845c0f63edbf2c27d87dd82e97
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2021-05-26 07:19:10 +0200

    [RORDEV-491] regression in multi-tenancy handling (#723)
    Fix (ES) regression in multi-tenancy handling

--- core/src/test/scala/tech/beshu/ror/integration/KibanaIndexAndAccessYamlLoadedAccessControlTests.scala
+++ core/src/test/scala/tech/beshu/ror/integration/KibanaIndexAndAccessYamlLoadedAccessControlTests.scala
@@ -23,8 +23,9 @@ import org.scalamock.scalatest.MockFactory
 import org.scalatest.Inside
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import tech.beshu.ror.accesscontrol.AccessControl.RegularRequestResult
+import tech.beshu.ror.accesscontrol.AccessControl.{RegularRequestResult, UserMetadataRequestResult}
 import tech.beshu.ror.accesscontrol.blocks.Block
+import tech.beshu.ror.accesscontrol.blocks.metadata.UserMetadata
 import tech.beshu.ror.accesscontrol.domain.LoggedUser.DirectlyLoggedUser
 import tech.beshu.ror.accesscontrol.domain._
 import tech.beshu.ror.mocks.MockRequestContext
@@ -45,12 +46,31 @@ class KibanaIndexAndAccessYamlLoadedAccessControlTests extends AnyWordSpec
       |    verbosity: error
       |    kibana_access: admin
       |    kibana_index: ".kibana_template"
+      |    auth_key: john:dev
       |
       |  - name: "Read-Write access with RoR custom kibana index"
       |    indices: [".kibana_ror_custom", ".reporting.kibana_ror_custom-*", "logstash*", "readonlyrest_audit-*"]
       |    kibana_index: ".kibana_ror_custom"
       |    kibana_access: rw
       |    groups: ["RW_ror_custom"]
+      |
+      |  - name: PERSONAL_GRP
+      |    groups: [ Personal ]
+      |    kibana_access: rw
+      |    kibana_hide_apps: [ "Enterprise Search|Overview", "Observability" ]
+      |    kibana_index: '.kibana_@{user}'
+      |
+      |  - name: ADMIN_GRP
+      |    groups: [ Administrators ]
+      |    kibana_access: admin
+      |    kibana_hide_apps: [ "Enterprise Search|Overview", "Observability" ]
+      |    kibana_index: '.kibana_admins'
+      |
+      |  - name: Infosec
+      |    groups: [ Infosec ]
+      |    kibana_access: rw
+      |    kibana_hide_apps: [ "Enterprise Search|Overview", "Observability" ]
+      |    kibana_index: .kibana_infosec
       |
       |  users:
       |  - username: testuser_ro_master
@@ -61,12 +81,21 @@ class KibanaIndexAndAccessYamlLoadedAccessControlTests extends AnyWordSpec
       |    auth_key: testuser_ro_master_rw_custom:XXXX
       |    groups: ["RO_master", "RW_ror_custom"]
       |
+      |  - username: admin
+      |    auth_key: admin:dev
+      |    groups: [Administrators, Infosec]
+      |
+      |  - username: user1
+      |    auth_key: user1:dev
+      |    groups: [Administrators, Personal, Infosec]
+      |
       """.stripMargin
 
   "An ACL" when {
     "kibana index and kibana access rules are used" should {
       "allow to proceed" in {
         val request = MockRequestContext.indices.copy(
+          headers = Set(basicAuthHeader("john:dev")),
           action = Action("indices:monitor/*"),
           filteredIndices = Set(IndexName(".readonlyrest"))
         )
@@ -77,6 +106,7 @@ class KibanaIndexAndAccessYamlLoadedAccessControlTests extends AnyWordSpec
         inside(result.result) { case RegularRequestResult.Allow(blockContext, block) =>
           block.name should be(Block.Name("Template Tenancy"))
           assertBlockContext(
+            loggedUser = Some(DirectlyLoggedUser(User.Id("john"))),
             kibanaIndex = Some(IndexName(".kibana_template")),
             kibanaAccess = Some(KibanaAccess.Admin),
             indices = Set(IndexName(".readonlyrest")),
@@ -108,6 +138,59 @@ class KibanaIndexAndAccessYamlLoadedAccessControlTests extends AnyWordSpec
             kibanaIndex = Some(IndexName(".kibana_ror_custom")),
             kibanaAccess = Some(KibanaAccess.RW),
             indices = Set(IndexName(".kibana_ror_custom")),
+          ) {
+            blockContext
+          }
+        }
+      }
+    }
+    "kibana index and kibana access rules are used (when current user metadata request was called first)" should {
+      "allow to proceed" in {
+        val loginRequest = MockRequestContext.metadata.copy(
+          headers = Set(basicAuthHeader("admin:dev"))
+        )
+
+        val loginResult = acl.handleMetadataRequest(loginRequest).runSyncUnsafe()
+
+        inside(loginResult.result) { case UserMetadataRequestResult.Allow(metadata, _) =>
+          metadata should be(UserMetadata(
+            loggedUser = Some(DirectlyLoggedUser(User.Id("admin"))),
+            currentGroup = Some(Group("Administrators")),
+            availableGroups = UniqueList.of(Group("Administrators"), Group("Infosec")),
+            kibanaIndex = Some(IndexName(".kibana_admins")),
+            kibanaTemplateIndex = None,
+            hiddenKibanaApps = Set(KibanaApp("Enterprise Search|Overview"), KibanaApp("Observability")),
+            kibanaAccess = Some(KibanaAccess.Admin),
+            userOrigin = None,
+            jwtToken = None
+          ))
+        }
+
+        val request = MockRequestContext.indices.copy(
+          headers = Set(
+            basicAuthHeader("admin:dev"),
+            Header(("x-ror-current-group", "Administrators"))
+          ),
+          uriPath = UriPath("/.kibana_admins/_create/index-pattern:3b2fa1b0-bcb2-11eb-a20e-8daf1d07a2b2"),
+          method = Method("PUT"),
+          action = Action("indices:data/write/index"),
+          isReadOnlyRequest = false,
+          filteredIndices = Set(IndexName(".kibana_admins"))
+        )
+
+        val result = acl.handleRegularRequest(loginRequest).runSyncUnsafe()
+
+        result.history should have size 4
+        inside(result.result) { case RegularRequestResult.Allow(blockContext, block) =>
+          block.name should be(Block.Name("ADMIN_GRP"))
+          assertBlockContext(
+            loggedUser = Some(DirectlyLoggedUser(User.Id("admin"))),
+            currentGroup = Some(Group("Administrators")),
+            availableGroups = UniqueList.of(Group("Administrators")),
+            kibanaIndex = Some(IndexName(".kibana_admins")),
+            kibanaAccess = Some(KibanaAccess.Admin),
+            indices = Set(IndexName(".kibana_admins")),
+            hiddenKibanaApps = Set(KibanaApp("Observability"), KibanaApp("Enterprise Search|Overview"))
           ) {
             blockContext
           }
