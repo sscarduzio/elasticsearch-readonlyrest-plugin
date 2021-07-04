b2858d5a415116db6996fb6feaf767b6eca14c67
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2021-07-04 20:26:52 +0200

    compilation fix

--- integration-tests/src/test/scala/tech/beshu/ror/integration/suites/ClosedIndicesSuite.scala
+++ integration-tests/src/test/scala/tech/beshu/ror/integration/suites/ClosedIndicesSuite.scala
@@ -20,7 +20,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import tech.beshu.ror.integration.suites.base.support.BaseSingleNodeEsClusterTest
 import tech.beshu.ror.utils.containers.EsContainerCreator
-import tech.beshu.ror.utils.elasticsearch.{DocumentManager, IndexManager, IndexManagerJ, SearchManager}
+import tech.beshu.ror.utils.elasticsearch.{DocumentManager, IndexManager, SearchManager}
 import tech.beshu.ror.utils.httpclient.RestClient
 
 
