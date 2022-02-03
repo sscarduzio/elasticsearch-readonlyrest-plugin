7c3e8da67bea133c02b42644896f758285399b2d
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-02-03 23:38:56 +0100

    eck cluster bootstrap cna cleanup stripts

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM docker.elastic.co/elasticsearch/elasticsearch:7.15.0
-COPY es714x/build/distributions/readonlyrest-1.38.0-pre2_es7.15.0.zip /tmp
-RUN bin/elasticsearch-plugin install -b file:///tmp/readonlyrest-1.38.0-pre2_es7.15.0.zip
---
+++ eck/eck-ror-bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+if ! command -v kind &> /dev/null
+then
+    echo "Cannot find 'kind' tool. Please follow the installation steps: https://github.com/kubernetes-sigs/kind#installation-and-usage" 
+    exit 1
+fi
+
+if ! command -v docker &> /dev/null
+then
+  echo "Cannot find 'docker'. Please follow the installation steps: https://docs.docker.com/engine/install/"
+  exit 2
+fi
+
+echo "CONFIGURING K8S CLUSTER ..."
+kind create cluster --name ror-eck --config kind-cluster/kind-cluster-config.yml
+
+echo "CONFIGURING ECK ..."
+docker cp kind-cluster/bootstrap-eck.sh ror-eck-control-plane:/
+docker exec ror-eck-control-plane chmod +x bootstrap-eck.sh
+docker exec ror-eck-control-plane ./bootstrap-eck.sh
+
+echo "CONFIGURING ES AND KBN WITH ROR ..."
+docker cp kind-cluster/ror ror-eck-control-plane:/
+docker exec ror-eck-control-plane bash -c 'cd ror && ls | xargs -n 1 kubectl apply -f'
+
+echo ""
+echo "------------------------------------------"
+echo "ECK and ROR is being bootstrapped. Wait for all pods to be run and then open your browser and try to access http://localhost:15601/ (credentials admin:container)"
+echo ""
+
+docker exec -ti ror-eck-control-plane kubectl get pods --watch
