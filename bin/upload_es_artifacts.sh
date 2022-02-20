ff70a1d2b432ab47050b989238afb4e3782d5c27
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-02-20 14:35:14 +0100

    updates

--- bin/upload_es_artifacts.sh
+++ bin/upload_es_artifacts.sh
@@ -4,4 +4,5 @@ set -xe
 
 echo ">>> ($0) UPLOADING ES ARTIFACTS ..."
 
+export GRADLE_OPTS="--add-modules java.xml.bind '-Dorg.gradle.jvmargs=--add-modules java.xml.bind'"
 ./gradlew --stacktrace --info clean es80x:publish
