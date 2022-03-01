6cd7c93c7adb4596f7cb25e23bdfebdc235beed5
sscarduzio <scarduzio@gmail.com>
2022-03-01 23:29:21 +0100

    test

--- ci/upload-file-to-s3.sh
+++ ci/upload-file-to-s3.sh
@@ -3,8 +3,9 @@
 source "$(dirname "$0")/ci-lib.sh"
 
 echo ">>>>>> DO WE HAVE AWS CREDS?"
-echo "Uploading $1 to $2 ..."
+echo "$aws_access_key_id" | cut -c1-6
 
+echo "Uploading $1 to $2 ..."
 upload $1 $2
 
 echo "DONE"
