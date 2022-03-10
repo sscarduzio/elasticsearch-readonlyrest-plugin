b473ad1c835e1c89d9abdeb7037433b0e69c3c6d
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-03-10 22:58:27 +0100

    added: 8.0.1

--- bin/upload_es_artifacts.sh
+++ bin/upload_es_artifacts.sh
@@ -5,3 +5,5 @@ set -xe
 echo ">>> ($0) UPLOADING ES ARTIFACTS ..."
 
 ./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.0.0
+./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.0.1
+./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.1.0
--- ci/upload-file-to-s3.sh
+++ ci/upload-file-to-s3.sh
@@ -2,9 +2,6 @@
 
 source "$(dirname "$0")/ci-lib.sh"
 
-echo ">>>>>> DO WE HAVE AWS CREDS?"
-echo "$aws_access_key_id" | cut -c1-6
-
 echo "Uploading $1 to $2 ..."
 upload $1 $2
 
