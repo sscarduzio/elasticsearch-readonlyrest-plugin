24635bddf2ed8c22046939d0de8bc6253820b1f8
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-06-28 22:53:03 +0200

    ES 8.3.0 support

--- bin/build.sh
+++ bin/build.sh
@@ -40,6 +40,11 @@ if [[ -z $TRAVIS ]] ||  [[ $ROR_TASK == "integration_proxy" ]]; then
     ./gradlew integration-tests:test '-PesModule=proxy' '-Pmode=proxy' || ( find . |grep hs_err |xargs cat && exit 1 )
 fi
 
+if [[ -z $TRAVIS ]] ||  [[ $ROR_TASK == "integration_es83x" ]]; then
+    echo ">>> es83x => Running testcontainers.."
+    ./gradlew integration-tests:test '-PesModule=es83x' '-Pmode=plugin' || ( find . |grep hs_err |xargs cat && exit 1 )
+fi
+
 if [[ -z $TRAVIS ]] ||  [[ $ROR_TASK == "integration_es82x" ]]; then
     echo ">>> es82x => Running testcontainers.."
     ./gradlew integration-tests:test '-PesModule=es82x' '-Pmode=plugin' || ( find . |grep hs_err |xargs cat && exit 1 )
@@ -153,6 +158,9 @@ fi
 if [[ -z $TRAVIS ]] ||  [[ $ROR_TASK == "package_es8xx" ]]; then
 
     echo ">>> ($0) additional builds of ES module for specified ES version"
+
+    #es83
+    ./gradlew --stacktrace es83x:ror '-PesVersion=8.3.0'
 
     #es82
     ./gradlew --stacktrace es82x:ror '-PesVersion=8.2.3'
---
+++ es83x/src/main/scala/tech/beshu/ror/es/utils/ThreadContextOps.scala
@@ -0,0 +1,55 @@
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+package tech.beshu.ror.es.utils
+
+import org.elasticsearch.common.util.concurrent.ThreadContext
+import tech.beshu.ror.accesscontrol.domain.Header
+import tech.beshu.ror.es.handler.AclAwareRequestFilter.EsContext
+import tech.beshu.ror.utils.JavaConverters
+
+import scala.language.implicitConversions
+
+final class ThreadContextOps(val threadContext: ThreadContext) extends AnyVal {
+
+  def stashAndMergeResponseHeaders(esContext: EsContext): ThreadContext.StoredContext = {
+    val responseHeaders =
+      JavaConverters.flattenPair(threadContext.getResponseHeaders).toSet ++ esContext.threadContextResponseHeaders
+    val storedContext = threadContext.stashContext()
+    responseHeaders.foreach { case (k, v) => threadContext.addResponseHeader(k, v) }
+    storedContext
+  }
+
+  def putHeaderIfNotPresent(header: Header): ThreadContext = {
+    Option(threadContext.getHeader(header.name.value.value)) match {
+      case Some(_) =>
+      case None => threadContext.putHeader(header.name.value.value, header.value.value)
+    }
+    threadContext
+  }
+
+  def addXPackAuthenticationHeader(nodeName: String): ThreadContext = {
+    putHeaderIfNotPresent(XPackSecurityAuthenticationHeader.createXpackAuthenticationHeader(nodeName))
+  }
+
+  def addSystemAuthenticationHeader(nodeName: String): ThreadContext = {
+    putHeaderIfNotPresent(XPackSecurityAuthenticationHeader.createSystemAuthenticationHeader(nodeName))
+  }
+}
+
+object ThreadContextOps {
+  implicit def createThreadContextOps(threadContext: ThreadContext): ThreadContextOps = new ThreadContextOps(threadContext)
+}
