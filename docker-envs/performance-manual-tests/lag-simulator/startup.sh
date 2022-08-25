cca23ed3bc0c0adf518afd7a019c1b2bf6d14292
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-08-25 21:24:37 +0200

    wip

--- docker-envs/performance-manual-tests/lag-simulator/startup.sh
+++ docker-envs/performance-manual-tests/lag-simulator/startup.sh
@@ -1,28 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 
-toxiproxy-cli create -l localhost:13890 -u localhost:3890 tolldap
-toxiproxy-cli toxic add -t latency -a latency=1000 tolldap
-
-## you will need to change hostname and ip to suite your needs
-#
-## destination ip (connect was a hostname of a docker container)
-#DESTINATION_IP=$(dig lldap +short)
-#echo
-## traffic control to create 100ms delay
-#tc qdisc add dev eth0 root netem delay 9000ms
-#
-## socat multipurpose relay
-#socat tcp-listen:13890,reuseaddr,fork tcp:$DESTINATION_IP:3890
-#
-#
-## change to random delay
-## tc qdisc change dev eth0 root netem delay 100ms 10ms
-#
-## delete
-## tc qdisc del dev eth0 root netem
-#
-## add 250ms delay
-## tc qdisc add dev eth0 root netem delay 250ms
-#
-## add random delay
-## tc qdisc add dev eth0 root netem delay 100ms 10ms
+toxiproxy-server &
+toxiproxy-cli create -l 0.0.0.0:13890 -u localhost:3890 tolldap
+toxiproxy-cli toxic add -t latency -a latency=5000 tolldap
+toxiproxy-cli list
