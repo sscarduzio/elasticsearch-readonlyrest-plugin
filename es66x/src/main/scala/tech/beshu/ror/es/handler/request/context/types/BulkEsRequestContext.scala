be2e8a5f9310be3d8cc0c6d88fb2d983503fe2cc
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-11-26 16:29:10 +0100

    fix

--- es66x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/BulkEsRequestContext.scala
+++ es66x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/BulkEsRequestContext.scala
@@ -26,6 +26,7 @@ import tech.beshu.ror.accesscontrol.blocks.BlockContext.MultiIndexRequestBlockCo
 import tech.beshu.ror.accesscontrol.blocks.BlockContext.MultiIndexRequestBlockContext.Indices
 import tech.beshu.ror.accesscontrol.blocks.metadata.UserMetadata
 import tech.beshu.ror.accesscontrol.domain
+import tech.beshu.ror.accesscontrol.domain.ClusterIndexName
 import tech.beshu.ror.es.RorClusterService
 import tech.beshu.ror.es.handler.AclAwareRequestFilter.EsContext
 import tech.beshu.ror.es.handler.request.context.ModificationResult.{Modified, ShouldBeInterrupted}
