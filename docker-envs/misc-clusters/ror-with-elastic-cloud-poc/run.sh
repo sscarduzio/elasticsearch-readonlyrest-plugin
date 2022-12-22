bd29fac1f43cbac67d2854de78a7d4d52276bafa
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2022-12-22 22:43:50 +0100

    wip

--- docker-envs/misc-clusters/ror-with-elastic-cloud-poc/run.sh
+++ docker-envs/misc-clusters/ror-with-elastic-cloud-poc/run.sh
@@ -1,42 +1,130 @@
 #!/bin/bash -e
 
-rm -rf .base
-cp -R ../../base-ror-docker .base
+if ! command -v docker-compose; then
+  echo "The script require docker-compose to be installed on your machine."
+  exit 1
+fi
+
+echo -e "
+
+  _____                _  ____        _       _____  ______  _____ _______
+ |  __ \              | |/ __ \      | |     |  __ \|  ____|/ ____|__   __|
+ | |__) |___  __ _  __| | |  | |_ __ | |_   _| |__) | |__  | (___    | |
+ |  _  // _ \/ _| |/ _| | |  | | '_ \| | | | |  _  /|  __|  \___ \   | |
+ | | \ \  __/ (_| | (_| | |__| | | | | | |_| | | \ \| |____ ____) |  | |
+ |_|  \_\___|\__,_|\__,_|\____/|_| |_|_|\__, |_|  \_\______|_____/   |_|
+                                         __/ |
+"
 
 echo -e "This tool will help you to bootstrap PoC of a local ROR cluster connected to a remote Elastic cloud cluster.
 Please, follow the instructions below ...\n"
 
-echo -e "Certificates for SSL transport will be generated. A connection between local ROR cluster
-and remote Elastic cluster will use two-way SSL. ROR needs CA of the Elastic cluster, and the Elastic cluster
-needs ROR's CA.\n"
+read -p "Are you ready? Enter anything to continue ..."
 
-echo -e "\nTo download your Elastic cloud cluster CA please:
+rm -rf .base
+cp -R ../../base-ror-docker .base
+
+echo -e "\nCertificates for SSL transport will be generated.
+A connection between local ROR cluster and remote Elastic cluster will use two-way SSL.
+ROR needs CA of the Elastic Cloud cluster, and the Elastic Cloud cluster needs ROR CA.
+ROR cluster is going to have one node 'ror-es01'"
+
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          DOWNLOADING ELASTIC CLOUD CLUSTER CA FILE              ***
+***                                                                 ***
+***********************************************************************
+"
+
+echo -e "To download your Elastic cloud cluster CA file, please:
 1. go to https://cloud.elastic.co/login and log in
-2. go to one of your deployments and click \"Manage deployments\" and go to \"Security\" tab
+2. go to your deployment and click \"Manage deployments\" and go to \"Security\" tab
 3. in \"CA certificates\" section you should find a CA cert that you should download
-4. change its name to \"ca.cer\" and put it in 'docker-envs/misc-clusters/ror-with-elastic-cloud-poc/cert-generator/certs/elastic-cloud'
-\n"
+"
 
-read -p "Are you ready to continue? We will generate ROR's certs. Enter anything to continue ..."
+while true; do
+  read -p "Enter the downloaded CA file absolute path: " elasticCloudCaFilePath
+  if [ -z $elasticCloudCaFilePath ]; then
+    echo "It cannot be empty. Please try again ..."
+    continue
+  fi
+  if [ -f $elasticCloudCaFilePath ]; then
+    cat $elasticCloudCaFilePath > certs-generator/input/elastic-cloud-ca.cer
+    break
+  else
+    echo "The CA file cannot be found. Please try again ..."
+    continue
+  fi
+done
 
-rm -rf certs # // todo:
+echo -e "instances:
+  - name: \"ror-es01\" #{node name}
+    cn:
+      - \"ror-es01.node.ror-cluster.ror-test\" #{node name}.node.{cluster name}.{scope}
+    dns:
+      - \"localhost\"
+    ip:
+      - \"127.0.0.1\"
+" > certs-generator/input/ror-cluster-instances.yml
+
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          GENERATING ROR CERTIFICATES AND ROR CA                 ***
+***                                                                 ***
+***********************************************************************
+"
+
+read -p "We have everything we need to generate ROR certificates and ROR CA. Enter anything to continue ..."
+
+rm -rf certs
 cd certs-generator
-./run.sh
+./run.sh > /dev/null 2>&1
 cp -R output ../certs
-./clean.sh
+./clean.sh > /dev/null 2>&1
 cd ..
 
-echo -e "\nCertificates should be generated. Now you have to continue the remote Elastic cluster.
+echo -e "\nCertificates and CA are generated:"
+echo -d "$(pwd)/certs"
+ls -l certs | grep -v '^total'
+
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          ADDING TRUSTED ENVIRONMENT IN ELASTIC CLOUD            ***
+***                                                                 ***
+***********************************************************************
+"
+
+echo -e "Now, we have to tell Elastic Cloud cluster that our cluster is trusted:
 1. go to https://cloud.elastic.co/login and log in
-2. go to one of your deployments and click \"Manage deployments\" and go to \"Security\" tab
+2. go to your deployment and click \"Manage deployments\" and go to \"Security\" tab
 3. in \"Trusted environments\" section click \"Add trusted environment\"
-4. Pick \"Self-managed\" option and:
-4a. in 1st input - drag and drop ROR's CA cert from 'docker-envs/misc-clusters/ror-with-elastic-cloud-poc/cert-generator/certs/ca/ca.crt'
-4b. in 2nd input - check \"Trust clusters whose Common Name follows the Elastic pattern\", enter 'ror-test' as Scope ID and check \"All deployments\"
-4c. in 3rd input - pick your environment name (anything you want)
-5. Click \"Create trust\" and then \"Done\" - ROR cluster should be added as trusted env
-6. in \"Remote cluster parameters\" you will find \"Proxy address\" and \"Server name\" - copy both of them and paste in 'docker-envs/misc-clusters/ror-with-elastic-cloud-poc/.env'
+4. Pick \"Self-managed\" option and click \"Next\":
+5. You will see a new page when you should configure a new trusted environment:
+5a. in 1st input - drag and drop ROR CA cert from '$(pwd)/certs/ca/ca.crt'
+5b. in 2nd input - check \"Trust clusters whose Common Name follows the Elastic pattern\", enter 'ror-test' as Scope ID and check \"All deployments\"
+5c. in 3rd input - pick your environment name (anything you want)
+6. Click \"Create trust\" and then \"Done\" - ROR cluster should be added as trusted environment
 \n"
+
+read -p "When all steps are done, please enter anything to continue ..."
+
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          CONFIGURING ELASTIC CLOUD AS THE REMOTE CLUSTER        ***
+***                                                                 ***
+***********************************************************************
+"
+
+echo -e "We're almost there. The last thing we need to do is configuring Elastic Cloud cluster as a remote cluster
+in our nodes settings. To do that we need to have a Proxy Address and Server Name:
+1. go to https://cloud.elastic.co/login and log in
+2. go to your deployment and click \"Manage deployments\" and go to \"Security\" tab
+3. in \"Remote cluster parameters\" section you will find \"Proxy address\" and \"Server name\"'
+"
 
 while true; do
   read -p "Enter \"Proxy address\": " proxyAddress
@@ -60,11 +148,27 @@ while true; do
   break
 done
 
-echo "Great. We will run the ROR cluster ..."
+echo "Great. This is all we need to run the ROR cluster ..."
+
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          RUNNING THE ROR CLUSTER                                ***
+***                                                                 ***
+***********************************************************************
+"
 
 ./.base/run.sh
 
-echo -e "\nEverything is done. Elastic cluster is configured to be a remote cluster for the ROR local cluster.\n\n."
+echo -e "\nEverything is done. Elastic cluster is configured to be a remote cluster for the ROR local cluster.\n"
 
-echo -e "You can access ROR KBN here: http://localhost:15601 (user1:test or admin:container)\n\n
+echo -e "
+***********************************************************************
+***                                                                 ***
+***          TIME TO PLAY!!!                                        ***
+***                                                                 ***
+***********************************************************************
+"
+
+echo -e "You can access ROR KBN here: http://localhost:15601 (regular user: 'user1:test' or admin user: 'admin:admin')\n\n
 Now, you can configure ROR settings and try to search remote cluster data."
