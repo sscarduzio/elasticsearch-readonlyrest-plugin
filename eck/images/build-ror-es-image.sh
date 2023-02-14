8286280f9b8a8e54910ecdd8b4038af0eb2a0855
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-02-14 22:00:04 +0100

    prepare for testing 8.6.1

--- eck/images/build-ror-es-image.sh
+++ eck/images/build-ror-es-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-docker build . -t es_7.17.0_ror_1.42.0-eck-poc -f Dockerfile-es
-docker tag es_7.17.0_ror_1.42.0-eck-poc coutopl/es_7.17.0_ror_1.42.0-eck-poc
-docker push coutopl/es_7.17.0_ror_1.42.0-eck-poc:latest
+docker build . -t es_8.6.1_ror_1.47.0-eck-poc -f Dockerfile-es
+docker tag es_8.6.1_ror_1.47.0-eck-poc coutopl/es_8.6.1_ror_1.47.0-eck-poc
+docker push coutopl/es_8.6.1_ror_1.47.0-eck-poc:latest
--- eck/kind-cluster/bootstrap-eck.sh
+++ eck/kind-cluster/bootstrap-eck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-kubectl create -f https://download.elastic.co/downloads/eck/1.9.1/crds.yaml
-kubectl apply -f https://download.elastic.co/downloads/eck/1.9.1/operator.yaml
+kubectl create -f https://download.elastic.co/downloads/eck/2.6.1/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.6.1/operator.yaml
