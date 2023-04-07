0dec8091606f219e05f690409a477b0347ae7227
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-04-07 17:31:30 +0200

    wip

--- eck/kind-cluster/bootstrap-eck.sh
+++ eck/kind-cluster/bootstrap-eck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-kubectl create -f https://download.elastic.co/downloads/eck/2.6.1/crds.yaml
-kubectl apply -f https://download.elastic.co/downloads/eck/2.6.1/operator.yaml
+kubectl create -f https://download.elastic.co/downloads/eck/2.6.2/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.6.2/operator.yaml
