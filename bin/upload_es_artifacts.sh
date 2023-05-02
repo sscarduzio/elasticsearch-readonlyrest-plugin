b33a97cada33ca40a5e64cbf2f27ae4e42c9df7d
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-05-02 23:22:29 +0200

    ES 8.7.1 & 7.17.10 support (#906)

--- bin/upload_es_artifacts.sh
+++ bin/upload_es_artifacts.sh
@@ -4,6 +4,7 @@ set -xe
 
 echo ">>> ($0) UPLOADING ES ARTIFACTS ..."
 
+./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.7.1
 ./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.7.0
 #./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.6.2
 #./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.6.1
