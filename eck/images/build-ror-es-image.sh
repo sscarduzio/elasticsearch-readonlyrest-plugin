f469d6a85ef8fc9662c135b39fb598055526b940
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-07-22 18:19:53 +0200

    eck kind cluster improvements

--- eck/images/build-ror-es-image.sh
+++ eck/images/build-ror-es-image.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build --platform=linux/arm64,linux/amd64 . -t es_8.8.2_ror_1.50.0-pre2 -f Dockerfile-es
-docker tag es_8.8.2_ror_1.50.0-pre2 coutopl/es_8.8.2_ror_1.50.0-pre2
-docker push coutopl/es_8.8.2_ror_1.50.0-pre2:latest
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/es_8.8.2_ror_1.50.0-pre2:latest -f Dockerfile-es .
--- eck/images/build-ror-kbn-image.sh
+++ eck/images/build-ror-kbn-image.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build --platform=linux/arm64,linux/amd64 . -t kbn_8.8.2_ror_1.49.1 -f Dockerfile-kbn
-docker tag kbn_8.8.2_ror_1.49.1 coutopl/kbn_8.8.2_ror_1.49.1
-docker push coutopl/kbn_8.8.2_ror_1.49.1:latest
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/kbn_8.8.2_ror_1.49.1:latest -f Dockerfile-kbn .
