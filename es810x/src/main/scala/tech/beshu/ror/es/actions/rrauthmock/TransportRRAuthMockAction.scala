79aa280556452afaedb1a5c3eef0f2a5001bac35
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-09-12 19:26:04 +0200

    new ES 8.10.0 support

---
+++ es810x/src/main/scala/tech/beshu/ror/es/actions/rrauthmock/TransportRRAuthMockAction.scala
@@ -0,0 +1,44 @@
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+package tech.beshu.ror.es.actions.rrauthmock
+
+import org.elasticsearch.action.ActionListener
+import org.elasticsearch.action.support.{ActionFilters, HandledTransportAction}
+import org.elasticsearch.common.inject.Inject
+import org.elasticsearch.tasks.Task
+import org.elasticsearch.transport.TransportService
+
+import scala.annotation.nowarn
+
+class TransportRRAuthMockAction(transportService: TransportService,
+                                actionFilters: ActionFilters,
+                                @nowarn("cat=unused") constructorDiscriminator: Unit)
+  extends HandledTransportAction[RRAuthMockRequest, RRAuthMockResponse](
+    RRAuthMockActionType.name, transportService, actionFilters, RRAuthMockActionType.exceptionReader
+  ) {
+
+  @Inject
+  def this(transportService: TransportService,
+           actionFilters: ActionFilters) =
+    this(transportService, actionFilters, ())
+
+  private val handler = new RRAuthMockActionHandler()
+
+  override def doExecute(task: Task, request: RRAuthMockRequest, listener: ActionListener[RRAuthMockResponse]): Unit = {
+    handler.handle(request, listener)
+  }
+}
