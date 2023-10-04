686e6c17be2f596df9e28080178a31103633b0c6
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-10-04 21:42:53 +0200

    improvements

--- docker-envs/build-ror-in-docker/build.sh
+++ docker-envs/build-ror-in-docker/build.sh
@@ -5,8 +5,8 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-ES_VERSION=$1
+ES_VERSIONS=$*
 
-docker build --no-cache --progress=plain -t ror-builder-tmp ../../
-docker run --rm -v $(pwd)/builds:/ror/builds ror-builder-tmp $ES_VERSION
+docker buildx build --no-cache --progress=plain --load -t ror-builder-tmp ../../
+docker run --rm -v $(pwd)/builds:/ror/builds ror-builder-tmp $ES_VERSIONS
 docker rmi -f ror-builder-tmp
