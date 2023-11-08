b23b0234893aaea7c9aea491e14040aa77b79b3e
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-11-08 21:49:00 +0100

    check ROR 1.50.0

--- eck/images/build-ror-es-image.sh
+++ eck/images/build-ror-es-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/es_7.17.13_ror_1.52.0:latest -f Dockerfile-es .
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/es_7.17.11_ror_1.50.0:latest -f Dockerfile-es .
