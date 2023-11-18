29aea98c7b68bdcde8db1e760a94f0afc591c0c8
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2023-11-18 18:52:06 +0100

    testing 8.10.4

--- eck/images/build-ror-kbn-image.sh
+++ eck/images/build-ror-kbn-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/kbn_7.17.11_ror_1.50.0:latest -f Dockerfile-kbn .
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t coutopl/kbn_8.10.4_ror_1.52.0:latest -f Dockerfile-kbn .
