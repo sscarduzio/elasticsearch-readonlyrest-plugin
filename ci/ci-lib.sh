cc42f803d40a9ef400e43963eb30f8f99fb18eb7
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-01-14 20:53:33 +0100

    wip

--- ci/ci-lib.sh
+++ ci/ci-lib.sh
@@ -6,7 +6,7 @@ function checkTagNotExist {
   GIT_TAG="$1"
 
   # Check if this tag already exists, so we don't overwrite builds
-  if git pull --tags && git tag --list | egrep -e "^${GIT_TAG}$" >/dev/null; then
+  if git pull --tags >/dev/null && git tag --list | egrep -e "^${GIT_TAG}$" >/dev/null; then
     echo "Git tag $GIT_TAG already exists, exiting."
     return 1
   fi
