d1fb050d23279c1a4d975dcb4a9f7db4953e4714
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-01-18 17:09:40 +0100

    [RORDEV-1080] EsAuditSinkService should not close NodeClient, but the bulk processor (#976)

--- es89x/src/main/scala/tech/beshu/ror/es/services/EsAuditSinkService.scala
+++ es89x/src/main/scala/tech/beshu/ror/es/services/EsAuditSinkService.scala
@@ -39,8 +39,8 @@ class EsAuditSinkService(client: Client)
     BulkProcessor
       .builder(BulkRequestHandler, new AuditSinkBulkProcessorListener, "ror-audit-bulk-processor")
       .setBulkActions(AUDIT_SINK_MAX_ITEMS)
-      .setBulkSize(new ByteSizeValue(AUDIT_SINK_MAX_KB.toInt, ByteSizeUnit.KB))
-      .setFlushInterval(TimeValue.timeValueSeconds(AUDIT_SINK_MAX_SECONDS.toInt))
+      .setBulkSize(new ByteSizeValue(AUDIT_SINK_MAX_KB, ByteSizeUnit.KB))
+      .setFlushInterval(TimeValue.timeValueSeconds(AUDIT_SINK_MAX_SECONDS))
       .setConcurrentRequests(1)
       .setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), AUDIT_SINK_MAX_RETRIES))
       .build
@@ -54,7 +54,7 @@ class EsAuditSinkService(client: Client)
   }
 
   override def close(): Unit = {
-    client.close()
+    bulkProcessor.close()
   }
 
   private object BulkRequestHandler extends BiConsumer[BulkRequest, ActionListener[BulkResponse]] {
