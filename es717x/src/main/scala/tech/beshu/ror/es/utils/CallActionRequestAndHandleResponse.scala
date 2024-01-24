231e7b62effe4450f07d49261d9bf68d17678207
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-01-24 17:47:46 +0100

    wip

--- es717x/src/main/scala/tech/beshu/ror/es/utils/CallActionRequestAndHandleResponse.scala
+++ es717x/src/main/scala/tech/beshu/ror/es/utils/CallActionRequestAndHandleResponse.scala
@@ -3,6 +3,7 @@ package tech.beshu.ror.es.utils
 import monix.eval.Task
 import monix.execution.atomic.Atomic
 import org.elasticsearch.action.{ActionListener, ActionRequest, ActionRequestBuilder, ActionResponse}
+import org.elasticsearch.client.Cancellable
 
 import scala.concurrent.Promise
 import scala.language.implicitConversions
@@ -10,34 +11,48 @@ import scala.language.implicitConversions
 class CallActionRequestAndHandleResponse[REQUEST <: ActionRequest, RESPONSE <: ActionResponse] private(builder: ActionRequestBuilder[REQUEST, RESPONSE]) {
 
   def call[R](f: RESPONSE => R): Task[R] = {
-    val listener = new GenericResponseListener()
+    val listener = new GenericResponseListener[RESPONSE]()
     builder.execute(listener)
     listener.result(f)
-  }
-
-  private final class GenericResponseListener extends ActionListener[RESPONSE] {
-
-    private val promise = Promise[RESPONSE]()
-    private val finalizer = Atomic(Task.unit)
-
-    def result[T](f: RESPONSE => T): Task[T] = Task
-      .fromFuture(promise.future)
-      .map(f)
-      .guarantee(finalizer.getAndSet(Task.unit))
-
-    override def onResponse(response: RESPONSE): Unit = {
-      response.incRef()
-      finalizer.set(Task.delay(response.decRef()))
-      promise.success(response)
-    }
-
-    override def onFailure(exception: Exception): Unit = {
-      promise.failure(exception)
-    }
   }
 }
 
 object CallActionRequestAndHandleResponse {
   implicit def toOps[REQUEST <: ActionRequest, RESPONSE <: ActionResponse](builder: ActionRequestBuilder[REQUEST, RESPONSE]): CallActionRequestAndHandleResponse[REQUEST, RESPONSE] =
     new CallActionRequestAndHandleResponse(builder)
+}
+
+class InvokeCallerAndHandleResponse[RESPONSE <: ActionResponse] private(caller: ActionListener[RESPONSE] => _) {
+
+  def execute[R](f: RESPONSE => R): Task[R] = {
+    val listener = new GenericResponseListener[RESPONSE]()
+    caller(listener)
+    listener.result(f)
+  }
+}
+
+object InvokeCallerAndHandleResponse {
+  implicit def toOps[RESPONSE <: ActionResponse](caller: ActionListener[RESPONSE] => Cancellable): InvokeCallerAndHandleResponse[RESPONSE] =
+    new InvokeCallerAndHandleResponse(caller)
+}
+
+private final class GenericResponseListener[RESPONSE <: ActionResponse] extends ActionListener[RESPONSE] {
+
+  private val promise = Promise[RESPONSE]()
+  private val finalizer = Atomic(Task.unit)
+
+  def result[T](f: RESPONSE => T): Task[T] = Task
+    .fromFuture(promise.future)
+    .map(f)
+    .guarantee(finalizer.getAndSet(Task.unit))
+
+  override def onResponse(response: RESPONSE): Unit = {
+    response.incRef()
+    finalizer.set(Task.delay(response.decRef()))
+    promise.success(response)
+  }
+
+  override def onFailure(exception: Exception): Unit = {
+    promise.failure(exception)
+  }
 }
