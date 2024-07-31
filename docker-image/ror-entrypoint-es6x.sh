73da1d90470c61d4e3697cc7a88c810c9043de86
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-07-31 22:24:11 +0200

    ECK-related docker images fix (#1034)

--- docker-image/ror-entrypoint-es6x.sh
+++ docker-image/ror-entrypoint-es6x.sh
@@ -5,13 +5,21 @@ INVOKE_ROR_TOOLS="$JAVA_HOME/bin/java -jar /usr/share/elasticsearch/plugins/read
 if $INVOKE_ROR_TOOLS verify | grep -q "Elasticsearch is NOT patched"; then
 
   if [ -n "$I_UNDERSTAND_IMPLICATION_OF_ES_PATCHING" ] && [[ "${I_UNDERSTAND_IMPLICATION_OF_ES_PATCHING,,}" == *"yes"* ]]; then
-    $INVOKE_ROR_TOOLS patch
+    if [ "$(id -u)" -ne 0 ]; then
+      echo "Elasticsearch needs to be patched to work with ReadonlyREST. You can read about patching in our documentation:" \
+           "https://docs.readonlyrest.com/elasticsearch#id-3.-patch-elasticsearch. To patch Elasticsearch the container " \
+           "has to be run as root. Then, after the patching step, the Elasticsearch process will be run as 'elasticsearch' " \
+           "user. "
+      exit 1
+    else
+      $INVOKE_ROR_TOOLS patch
+    fi
   else
     echo "Elasticsearch needs to be patched to work with ReadonlyREST. You can read about patching in our documentation:" \
          "https://docs.readonlyrest.com/elasticsearch#id-3.-patch-elasticsearch. In this Docker image, the patching step" \
          "is done automatically, but you must agree to have it done for you. You can do this by setting" \
          "I_UNDERSTAND_IMPLICATION_OF_ES_PATCHING=yes environment variable when running the container."
-    exit 1
+    exit 2
   fi
 
 else
