cc352d9b98ac1b141b19de36d5333e9c6da998ff
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-11-12 22:49:51 +0100

    ES 8.16.0 support

---
+++ es816x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/snapshots/RestoreSnapshotEsRequestContext.scala
@@ -0,0 +1,115 @@
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+package tech.beshu.ror.es.handler.request.context.types.snapshots
+
+import cats.data.NonEmptyList
+import cats.implicits.*
+import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotRequest
+import org.elasticsearch.threadpool.ThreadPool
+import tech.beshu.ror.accesscontrol.blocks.BlockContext
+import tech.beshu.ror.accesscontrol.blocks.BlockContext.SnapshotRequestBlockContext
+import tech.beshu.ror.accesscontrol.domain
+import tech.beshu.ror.accesscontrol.domain.{ClusterIndexName, RepositoryName, RequestedIndex, SnapshotName}
+import tech.beshu.ror.es.RorClusterService
+import tech.beshu.ror.es.handler.AclAwareRequestFilter.EsContext
+import tech.beshu.ror.es.handler.RequestSeemsToBeInvalid
+import tech.beshu.ror.es.handler.request.context.ModificationResult
+import tech.beshu.ror.es.handler.request.context.types.BaseSnapshotEsRequestContext
+import tech.beshu.ror.implicits.*
+import tech.beshu.ror.syntax.*
+import tech.beshu.ror.utils.ScalaOps.*
+
+class RestoreSnapshotEsRequestContext(actionRequest: RestoreSnapshotRequest,
+                                      esContext: EsContext,
+                                      clusterService: RorClusterService,
+                                      override val threadPool: ThreadPool)
+  extends BaseSnapshotEsRequestContext[RestoreSnapshotRequest](actionRequest, esContext, clusterService, threadPool) {
+
+  override protected def snapshotsFrom(request: RestoreSnapshotRequest): Set[SnapshotName] =
+    SnapshotName
+      .from(request.snapshot())
+      .toCovariantSet
+
+  override protected def repositoriesFrom(request: RestoreSnapshotRequest): Set[RepositoryName] = Set {
+    RepositoryName
+      .from(request.repository())
+      .getOrElse(throw RequestSeemsToBeInvalid[RestoreSnapshotRequest]("Repository name is empty"))
+  }
+
+  override protected def requestedIndicesFrom(request: RestoreSnapshotRequest): Set[RequestedIndex[ClusterIndexName]] =
+    request
+      .indices.asSafeSet
+      .flatMap(RequestedIndex.fromString)
+      .orWildcardWhenEmpty
+
+  override protected def modifyRequest(blockContext: BlockContext.SnapshotRequestBlockContext): ModificationResult = {
+    val updateResult = for {
+      snapshots <- snapshotFrom(blockContext)
+      repository <- repositoryFrom(blockContext)
+      indices <- indicesFrom(blockContext)
+    } yield update(actionRequest, snapshots, repository, indices)
+    updateResult match {
+      case Right(_) =>
+        ModificationResult.Modified
+      case Left(_) =>
+        logger.error(s"[${id.show}] Cannot update ${actionRequest.getClass.show} request. It's safer to forbid the request, but it looks like an issue. Please, report it as soon as possible.")
+        ModificationResult.ShouldBeInterrupted
+    }
+  }
+
+  private def snapshotFrom(blockContext: SnapshotRequestBlockContext) = {
+    val snapshots = blockContext.snapshots.toList
+    snapshots match {
+      case Nil =>
+        Left(())
+      case snapshot :: rest =>
+        if (rest.nonEmpty) {
+          logger.warn(s"[${blockContext.requestContext.id.show}] Filtered result contains more than one snapshot. First was taken. The whole set of repositories [${snapshots.show}]")
+        }
+        Right(snapshot)
+    }
+  }
+
+  private def repositoryFrom(blockContext: SnapshotRequestBlockContext) = {
+    val repositories = blockContext.repositories.toList
+    repositories match {
+      case Nil =>
+        Left(())
+      case repository :: rest =>
+        if (rest.nonEmpty) {
+          logger.warn(s"[${blockContext.requestContext.id.show}] Filtered result contains more than one repository. First was taken. The whole set of repositories [${repositories.show}]")
+        }
+        Right(repository)
+    }
+  }
+
+  private def indicesFrom(blockContext: SnapshotRequestBlockContext) = {
+    NonEmptyList.fromList(blockContext.filteredIndices.toList) match {
+      case None => Left(())
+      case Some(indices) => Right(indices)
+    }
+  }
+
+  private def update(request: RestoreSnapshotRequest,
+                     snapshot: SnapshotName,
+                     repository: RepositoryName,
+                     indices: NonEmptyList[RequestedIndex[ClusterIndexName]]) = {
+    request.snapshot(SnapshotName.toString(snapshot))
+    request.repository(RepositoryName.toString(repository))
+    request.indices(indices.stringify: _*)
+  }
+}
---
+++ es816x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/templates/DeleteTemplateEsRequestContext.scala
@@ -0,0 +1,70 @@
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+package tech.beshu.ror.es.handler.request.context.types.templates
+
+import cats.data.NonEmptyList
+import cats.implicits.*
+import org.elasticsearch.action.admin.indices.template.delete.DeleteIndexTemplateRequest
+import org.elasticsearch.threadpool.ThreadPool
+import tech.beshu.ror.accesscontrol.blocks.BlockContext.TemplateRequestBlockContext
+import tech.beshu.ror.accesscontrol.domain.TemplateNamePattern
+import tech.beshu.ror.accesscontrol.domain.TemplateOperation.DeletingLegacyTemplates
+import tech.beshu.ror.es.RorClusterService
+import tech.beshu.ror.es.handler.AclAwareRequestFilter.EsContext
+import tech.beshu.ror.es.handler.RequestSeemsToBeInvalid
+import tech.beshu.ror.es.handler.request.context.ModificationResult
+import tech.beshu.ror.es.handler.request.context.types.BaseTemplatesEsRequestContext
+import tech.beshu.ror.implicits.*
+import tech.beshu.ror.syntax.*
+import tech.beshu.ror.utils.RefinedUtils.*
+import tech.beshu.ror.utils.ScalaOps.*
+
+class DeleteTemplateEsRequestContext(actionRequest: DeleteIndexTemplateRequest,
+                                     esContext: EsContext,
+                                     clusterService: RorClusterService,
+                                     override val threadPool: ThreadPool)
+  extends BaseTemplatesEsRequestContext[DeleteIndexTemplateRequest, DeletingLegacyTemplates](
+    actionRequest, esContext, clusterService, threadPool
+  ) {
+
+  override protected def templateOperationFrom(request: DeleteIndexTemplateRequest): DeletingLegacyTemplates = {
+    TemplateNamePattern.fromString(request.name()) match {
+      case Some(pattern) => DeletingLegacyTemplates(NonEmptyList.one(pattern))
+      case None => throw RequestSeemsToBeInvalid[DeleteIndexTemplateRequest]("No template name patterns found")
+    }
+  }
+
+  override protected def modifyRequest(blockContext: TemplateRequestBlockContext): ModificationResult = {
+    blockContext.templateOperation match {
+      case DeletingLegacyTemplates(namePatterns) =>
+        namePatterns.tail match {
+          case Nil =>
+          case _ =>
+            logger.warn(
+              s"""[${id.show}] Filtered result contains more than one template pattern. First was taken.
+                 | The whole set of patterns [${namePatterns.show}]""".oneLiner)
+        }
+        actionRequest.name(namePatterns.head.value.value)
+        ModificationResult.Modified
+      case other =>
+        logger.error(
+          s"""[${id.show}] Cannot modify templates request because of invalid operation returned by ACL (operation
+             | type [${other.getClass.show}]]. Please report the issue!""".oneLiner)
+        ModificationResult.ShouldBeInterrupted
+    }
+  }
+}
