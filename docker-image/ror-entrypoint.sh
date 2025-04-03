00765e68a783dfbfae67546050142696bdb592c0
Michał Goworko <m.goworko.pl@gmail.com>
2025-04-03 21:33:23 +0200

    missing flag renames

--- docker-image/ror-entrypoint.sh
+++ docker-image/ror-entrypoint.sh
@@ -26,7 +26,7 @@ if $INVOKE_ROR_TOOLS verify | grep -q "Elasticsearch is NOT patched"; then
     echo "Elasticsearch needs to be patched to work with ReadonlyREST. You can read about patching in our documentation:" \
          "https://docs.readonlyrest.com/elasticsearch#id-3.-patch-elasticsearch. In this Docker image, the patching step" \
          "is done automatically, but you must agree to have it done for you. You can do this by setting" \
-         "I_UNDERSTAND_IMPLICATION_OF_ES_PATCHING=yes environment variable when running the container."
+         "I_UNDERSTAND_AND_ACCEPT_ES_PATCHING=yes environment variable when running the container."
     exit 2
   fi
 
