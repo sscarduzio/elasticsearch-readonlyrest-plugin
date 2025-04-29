76fbbb1b8c5679d88ec258e0fad2ec2333e0d570
Michał Goworko <m.goworko.pl@gmail.com>
2025-04-29 17:25:17 +0200

    debug pipeline problem

--- ror-tools-core/src/main/scala/tech/beshu/ror/tools/core/patches/internal/filePatchers/JarManifestModifier.scala
+++ ror-tools-core/src/main/scala/tech/beshu/ror/tools/core/patches/internal/filePatchers/JarManifestModifier.scala
@@ -19,11 +19,9 @@ package tech.beshu.ror.tools.core.patches.internal.filePatchers
 import better.files.*
 import tech.beshu.ror.tools.core.utils.EsDirectory
 
-import java.util.UUID
-import java.util.jar.{JarEntry, JarOutputStream}
-import java.util.zip.{ZipFile, ZipInputStream}
-import scala.jdk.CollectionConverters.IteratorHasAsScala
-import scala.util.matching.Regex
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.util.jar.{JarFile, JarOutputStream, Manifest}
 import scala.util.{Failure, Success, Try}
 
 object JarManifestModifier {
@@ -31,66 +29,37 @@ object JarManifestModifier {
   val patchedByRorVersionPropertyName = "Patched-By-Ror-Version"
 
   def addPatchedByRorVersionProperty(jarFile: File, rorVersion: String): Unit = {
-    val tempJarFile = File(s"temp-${UUID.randomUUID()}.jar")
-    val zipFile = new ZipFile(jarFile.toJava)
-    val zipInput = new ZipInputStream(jarFile.newInputStream)
-    val jarOutput = new JarOutputStream(tempJarFile.newOutputStream.buffered)
-    try {
-      zipFile
-        .entries()
-        .asIterator()
-        .asScala
-        .foreach { entry =>
-          val name = entry.getName
-          if (name.equalsIgnoreCase("META-INF/MANIFEST.MF")) {
-            // Modify manifest file by adding new property and copy the result to the temp jar
-            val originalManifestContent = zipFile.getInputStream(entry).asString()
-            val customPropertyLine = s"$patchedByRorVersionPropertyName: $rorVersion\n"
-            val newManifestContent = originalManifestContent.replaceAll("(\\r\\n)+$", "") + "\n" + customPropertyLine
-            val newEntry = new JarEntry(name)
-            jarOutput.putNextEntry(newEntry)
-            jarOutput.write(newManifestContent.getBytes("UTF-8"))
-            jarOutput.closeEntry()
-          } else {
-            // Copy file other than manifest without changes to the temp jar
-            val newEntry = new JarEntry(name)
-            jarOutput.putNextEntry(newEntry)
-            val in = zipFile.getInputStream(entry)
-            in.transferTo(jarOutput)
-            in.close()
-            jarOutput.closeEntry()
-          }
-        }
-      val copyOptions: File.CopyOptions = File.CopyOptions(overwrite = true)
-      // Replace original jar with the newly created modified jar
-      tempJarFile.moveTo(jarFile)(copyOptions)
-    } finally {
-      zipInput.close()
-      jarOutput.close()
+    val originalJar = new JarFile(jarFile.toJava)
+    val manifest = new Manifest(originalJar.getManifest)
+    manifest.getMainAttributes.putValue(patchedByRorVersionPropertyName, rorVersion)
+    val tempJarFile = Files.createTempFile("updated-", ".jar").toFile
+    val jos = new JarOutputStream(new FileOutputStream(tempJarFile), manifest)
+    val entries = originalJar.entries()
+    while (entries.hasMoreElements) {
+      val entry = entries.nextElement()
+      if (!entry.getName.equalsIgnoreCase("META-INF/MANIFEST.MF")) {
+        val newEntry = new java.util.zip.ZipEntry(entry.getName)
+        jos.putNextEntry(newEntry)
+        val is = originalJar.getInputStream(entry)
+        is.transferTo(jos)
+        is.close()
+        jos.closeEntry()
+      }
     }
+    jos.close()
+    originalJar.close()
+    Files.move(tempJarFile.toPath, jarFile.toJava.toPath, java.nio.file.StandardCopyOption.REPLACE_EXISTING)
   }
 
   def findPatchedFiles(esDirectory: EsDirectory): List[PatchedJarFile] = {
     val directory = File(esDirectory.modulesPath.wrapped)
-    directory.walk().filter(_.name.endsWith(".jar")).toList.flatMap { jarFile =>
-      Try(new ZipFile(jarFile.toJava)) match {
-        case Success(zipFile) =>
-          val zipInput = new ZipInputStream(jarFile.newInputStream)
-          val patchedFiles =
-            zipFile
-              .entries()
-              .asIterator()
-              .asScala
-              .find(_.getName.equalsIgnoreCase("META-INF/MANIFEST.MF"))
-              .flatMap { manifestFile =>
-                val manifestContentLines = zipFile.getInputStream(manifestFile).asString().linesIterator.toList.map(_.strip)
-                val pattern: Regex = s"""$patchedByRorVersionPropertyName: (.*)""".r
-                manifestContentLines.collectFirst { case pattern(rorVersion) => PatchedJarFile(jarFile.name, rorVersion) }
-              }
-          zipInput.close()
-          patchedFiles
+    directory.walk().filter(_.name.endsWith(".jar")).toList.flatMap { file =>
+      Try(new JarFile(file.toJava)) match {
+        case Success(jarFile) =>
+          val rorVersion = Option(jarFile.getManifest.getMainAttributes.getValue(patchedByRorVersionPropertyName))
+          rorVersion.map(PatchedJarFile(file.name, _))
         case Failure(exception) =>
-          Console.err.println(jarFile)
+          Console.err.println(file)
           Console.err.println(exception)
           None
       }
