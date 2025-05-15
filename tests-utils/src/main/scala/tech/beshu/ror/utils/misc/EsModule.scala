13ec08ae637211fee5b95e217e10441c0bbd42b7
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2025-05-15 20:28:00 +0200

    Field caps test disabled for es711x and es714x modules, because of non-determinism

--- tests-utils/src/main/scala/tech/beshu/ror/utils/misc/EsModule.scala
+++ tests-utils/src/main/scala/tech/beshu/ror/utils/misc/EsModule.scala
@@ -61,11 +61,11 @@ trait EsModulePatterns {
   val allEs7xBelowEs716x = "^es7(0?[0-9]|1[0-5])x$".r
   val allEs7xBelowEs714x = "^es7(0?[0-9]|1[0-3])x$".r
   val allEs7xBelowEs711x = "^es7(0?[0-9]|10)x$".r
+  val allEs7xBetweenEs711xAnd715x = "^es71[1-5]x$".r
   val allEs8x = "^es8\\d+x$".r
   val allEs9x = "^es9\\d+x$".r
   val allEs8xBelowEs87x = "^es8[0-6]x$".r
   val allEs8xAboveEs86x = "^es8([7-9]|[1-9][0-9])*x$".r
   val allEs8xBelowEs811x = "^es8([0-9]|10)x$".r
   val allES8xAboveEs815x = "^es8(1[5-9]|[2-9][0-9]|[1-9][0-9]{2,})x$".r
-
 }
