3e71a00b4d4f199dddce71c0299eb0965e95f616
Michał Goworko <m.goworko.pl@gmail.com>
2025-10-17 21:35:06 +0200

    unit tests

--- core/src/test/scala/tech/beshu/ror/unit/acl/factory/AuditSettingsTests.scala
+++ core/src/test/scala/tech/beshu/ror/unit/acl/factory/AuditSettingsTests.scala
@@ -514,7 +514,7 @@ class AuditSettingsTests extends AnyWordSpec with Inside {
               expectedAuditCluster = LocalAuditCluster
             )
           }
-          "custom serializer is set" in {
+          "QueryAuditLogSerializer serializer is set" in {
             val config = rorConfigFromUnsafe(
               """
                 |readonlyrest:
@@ -537,6 +537,72 @@ class AuditSettingsTests extends AnyWordSpec with Inside {
               expectedIndexName = "readonlyrest_audit-2018-12-31",
               expectedAuditCluster = LocalAuditCluster
             )
+          }
+          "QueryAuditLogSerializer serializer is set and correctly serializes event without logged user" in {
+            val config = rorConfigFromUnsafe(
+              """
+                |readonlyrest:
+                |  audit:
+                |    enabled: true
+                |    outputs:
+                |    - type: index
+                |      serializer: "tech.beshu.ror.audit.instances.QueryAuditLogSerializer"
+                |
+                |  access_control_rules:
+                |
+                |  - name: test_block
+                |    type: allow
+                |    auth_key: admin:container
+                |
+              """.stripMargin)
+
+            assertIndexBasedAuditSinkSettingsPresent[QueryAuditLogSerializer](
+              config,
+              expectedIndexName = "readonlyrest_audit-2018-12-31",
+              expectedAuditCluster = LocalAuditCluster
+            )
+            val createdSerializer = serializer(config)
+            val serializedResponse = createdSerializer.onResponse(
+              AuditResponseContext.Forbidden(new DummyAuditRequestContext(username = None))
+            )
+
+            serializedResponse shouldBe defined
+            serializedResponse.get.get("user") shouldBe "Bearer 123"
+            serializedResponse.get.get("presented_identity") shouldBe "Bearer 123"
+            serializedResponse.get.isNull("logged_user")
+          }
+          "QueryAuditLogSerializer serializer is set and correctly serializes event with logged user" in {
+            val config = rorConfigFromUnsafe(
+              """
+                |readonlyrest:
+                |  audit:
+                |    enabled: true
+                |    outputs:
+                |    - type: index
+                |      serializer: "tech.beshu.ror.audit.instances.QueryAuditLogSerializer"
+                |
+                |  access_control_rules:
+                |
+                |  - name: test_block
+                |    type: allow
+                |    auth_key: admin:container
+                |
+              """.stripMargin)
+
+            assertIndexBasedAuditSinkSettingsPresent[QueryAuditLogSerializer](
+              config,
+              expectedIndexName = "readonlyrest_audit-2018-12-31",
+              expectedAuditCluster = LocalAuditCluster
+            )
+            val createdSerializer = serializer(config)
+            val serializedResponse = createdSerializer.onResponse(
+              AuditResponseContext.Forbidden(new DummyAuditRequestContext(username = Some("my_user")))
+            )
+
+            serializedResponse shouldBe defined
+            serializedResponse.get.get("user") shouldBe "my_user"
+            serializedResponse.get.get("presented_identity") shouldBe "my_user"
+            serializedResponse.get.get("logged_user") shouldBe "my_user"
           }
           "custom environment-aware serializer is set and correctly serializes events" in {
             val config = rorConfigFromUnsafe(
@@ -562,7 +628,7 @@ class AuditSettingsTests extends AnyWordSpec with Inside {
               expectedAuditCluster = LocalAuditCluster
             )
             val createdSerializer = serializer(config)
-            val serializedResponse = createdSerializer.onResponse(AuditResponseContext.Forbidden(DummyAuditRequestContext))
+            val serializedResponse = createdSerializer.onResponse(AuditResponseContext.Forbidden(new DummyAuditRequestContext))
 
             serializedResponse shouldBe defined
             serializedResponse.get.get("custom_field_for_es_node_name") shouldBe "testEsNode"
@@ -841,13 +907,13 @@ class AuditSettingsTests extends AnyWordSpec with Inside {
           "ES version is greater than or equal 7.9.0" in {
             val esVersions =
               List(
-                EsVersion(8,17,0),
-                EsVersion(8,1,0),
-                EsVersion(8,0,0),
-                EsVersion(7,17,27),
-                EsVersion(7,10,0),
-                EsVersion(7,9,1),
-                EsVersion(7,9,0),
+                EsVersion(8, 17, 0),
+                EsVersion(8, 1, 0),
+                EsVersion(8, 0, 0),
+                EsVersion(7, 17, 27),
+                EsVersion(7, 10, 0),
+                EsVersion(7, 9, 1),
+                EsVersion(7, 9, 0),
               )
 
             val config = rorConfigFromUnsafe(
@@ -1969,7 +2035,7 @@ private class TestEnvironmentAwareAuditLogSerializer extends EnvironmentAwareAud
 
 }
 
-private object DummyAuditRequestContext extends AuditRequestContext {
+private class DummyAuditRequestContext(username: Option[String] = Some("logged_user")) extends AuditRequestContext {
   override def timestamp: Instant = Instant.now()
 
   override def id: String = ""
@@ -2002,7 +2068,7 @@ private object DummyAuditRequestContext extends AuditRequestContext {
 
   override def httpMethod: String = ""
 
-  override def loggedInUserName: Option[String] = Some("logged_user")
+  override def loggedInUserName: Option[String] = username
 
   override def impersonatedByUserName: Option[String] = None
 
@@ -2010,7 +2076,7 @@ private object DummyAuditRequestContext extends AuditRequestContext {
 
   override def attemptedUserName: Option[String] = None
 
-  override def rawAuthHeader: Option[String] = None
+  override def rawAuthHeader: Option[String] = Some("Bearer 123")
 
   override def generalAuditEvents: JSONObject = new JSONObject
 
