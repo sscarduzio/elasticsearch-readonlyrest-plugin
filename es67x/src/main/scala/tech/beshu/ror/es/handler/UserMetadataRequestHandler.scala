6a77a896640dfec176aeff92d15e1cccabec59c2
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2026-01-25 00:15:52 +0100

    fix

--- es67x/src/main/scala/tech/beshu/ror/es/handler/UserMetadataRequestHandler.scala
+++ es67x/src/main/scala/tech/beshu/ror/es/handler/UserMetadataRequestHandler.scala
@@ -107,8 +107,10 @@ private class RRMetadataResponse(apiVersion: UserMetadataApiVersion,
                                  correlationId: CorrelationId)
   extends ActionResponse with ToXContentObject {
 
+  import tech.beshu.ror.utils.CirceOps.toJava
+
   override def toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder = {
-    val json = MetadataResponse.from(userMetadata, currentGroupId, correlationId)
+    val json = MetadataResponse.from(apiVersion, userMetadata, currentGroupId, correlationId)
     val javaMap = json.toJava.asInstanceOf[java.util.Map[String, _]]
     builder.map(javaMap)
     builder
