798fe1d799c51b43b33304d6e89c71ca41c15230
Michał Goworko <mgw@touk.pl>
2026-03-10 01:06:34 +0100

    adjust tests

---
+++ core/src/test/scala/tech/beshu/ror/unit/utils/DateTimePatternWildcardReplacementUtilTests.scala
@@ -0,0 +1,67 @@
+/*
+ *    This file is part of ReadonlyREST.
+ *
+ *    ReadonlyREST is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ReadonlyREST is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ReadonlyREST.  If not, see http://www.gnu.org/licenses/
+ */
+package tech.beshu.ror.unit.utils
+
+import cats.implicits.toShow
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+import tech.beshu.ror.constants.AUDIT_LOG_DEFAULT_INDEX_TEMPLATE
+import tech.beshu.ror.implicits.*
+import tech.beshu.ror.utils.NonEmptyStringUtils.*
+import tech.beshu.ror.utils.RefinedUtils.nes
+
+class DateTimePatternWildcardReplacementUtilTests extends AnyWordSpec {
+
+  "DateTime pattern replacement util" should {
+    "return correct pattern for template" when {
+      "default template" in {
+        val patternAfterReplacement = AUDIT_LOG_DEFAULT_INDEX_TEMPLATE.replaceDateTimePatternWithWildcard.toOption.get
+        patternAfterReplacement.show should be("readonlyrest_audit-*")
+      }
+      "monthly pattern that is included in docs" in {
+        val patternAfterReplacement = nes("'custom-prefix'-yyyy-MM").replaceDateTimePatternWithWildcard.toOption.get
+        patternAfterReplacement.show should be("custom-prefix-*")
+      }
+      "pattern in the middle" in {
+        val patternAfterReplacement = nes("'custom-prefix'-yyyy-MM'some-suffix").replaceDateTimePatternWithWildcard.toOption.get
+        patternAfterReplacement.show should be("custom-prefix-*some-suffix")
+      }
+    }
+    "return error" when {
+      "the name pattern is empty after resolving" in {
+        val patternAfterReplacement = nes("''").replaceDateTimePatternWithWildcard.left.toOption.get
+        patternAfterReplacement should be(DateTimePatternReplacementError.PatternResolvingResultsInEmptyString)
+      }
+      "the name pattern contains only whitespace after resolving (1)" in {
+        val patternAfterReplacement = nes(" ''").replaceDateTimePatternWithWildcard.left.toOption.get
+        patternAfterReplacement should be(DateTimePatternReplacementError.PatternResolvingResultsInEmptyString)
+      }
+      "the name pattern contains only whitespace after resolving (2)" in {
+        val patternAfterReplacement = nes("  ").replaceDateTimePatternWithWildcard.left.toOption.get
+        patternAfterReplacement should be(DateTimePatternReplacementError.PatternResolvingResultsInEmptyString)
+      }
+      "the name pattern contains only whitespace after resolving (3)" in {
+        val patternAfterReplacement = nes("'' ").replaceDateTimePatternWithWildcard.left.toOption.get
+        patternAfterReplacement should be(DateTimePatternReplacementError.PatternResolvingResultsInEmptyString)
+      }
+      "incomplete string literal in pattern" in {
+        val patternAfterReplacement = nes("'").replaceDateTimePatternWithWildcard.left.toOption.get
+        patternAfterReplacement should be(DateTimePatternReplacementError.IncompleteStringLiteralPresent)
+      }
+    }
+  }
+}
