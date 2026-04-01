ffe75cd5ef74064881d769de4f95451c9425f5da
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2026-04-01 14:35:16 +0200

    [RORDEV-1925] Make S3 upload destinations configurable in CI scripts (#1231)

--- ci/upload-report.sh
+++ ci/upload-report.sh
@@ -8,15 +8,13 @@ INT_TEST_REPORT=$(find . | grep report | grep integration | grep index.html)
 if [ ! -z "$UNIT_TEST_REPORT" ]; then
   echo "found unit test report UNIT_TEST_REPORT"
   S3_PATH="test_reports/$(date +%Y-%m-%d_%H.%M)$ROR_TASK-unit.html"
-  upload_to_ror_data_bucket $UNIT_TEST_REPORT $S3_PATH
-  upload_to_ror_data_xdelta_bucket $UNIT_TEST_REPORT $S3_PATH
+  upload_using_deltaglider_uploader $UNIT_TEST_REPORT $S3_PATH
 fi
 
 
 if [ ! -z "$INT_TEST_REPORT" ]; then
   echo "found integration test report $INT_TEST_REPORT"
   S3_PATH="test_reports/$(date +%Y-%m-%d_%H.%M)_$ROR_TASK-integration.html"
-  upload_to_ror_data_bucket $INT_TEST_REPORT $S3_PATH
-  upload_to_ror_data_xdelta_bucket $INT_TEST_REPORT $S3_PATH
+  upload_using_deltaglider_uploader $INT_TEST_REPORT $S3_PATH
 fi
 
