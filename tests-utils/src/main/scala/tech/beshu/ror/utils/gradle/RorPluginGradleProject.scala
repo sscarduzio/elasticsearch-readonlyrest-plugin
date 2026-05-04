55595241baea249bf1704cc514f2699f712dfc78
Mateusz Kołodziejczyk <coutopl@gmail.com>
2026-05-04 15:26:59 +0200

    fix

--- tests-utils/src/main/scala/tech/beshu/ror/utils/gradle/RorPluginGradleProject.scala
+++ tests-utils/src/main/scala/tech/beshu/ror/utils/gradle/RorPluginGradleProject.scala
@@ -69,7 +69,9 @@ class RorPluginGradleProject(val moduleName: String) extends LazyLogging {
     else Some(plugin)
   }
 
-  def getModuleESVersion: String = esProjectProperties.getProperty("latestSupportedEsVersion")
+  def getModuleESVersion: String =
+    Option(System.getProperty("esVersion"))
+      .getOrElse(esProjectProperties.getProperty("latestSupportedEsVersion"))
 
   private def esProject(esProjectName: String) = new JFile(RorPluginGradleProject.getRootProject, esProjectName)
 
@@ -79,7 +81,8 @@ class RorPluginGradleProject(val moduleName: String) extends LazyLogging {
   private def runTask(task: String): Unit = {
     val connector = GradleConnector.newConnector.forProjectDirectory(RorPluginGradleProject.getRootProject)
     val connect = Try(connector.connect())
-    val result = connect.map(_.newBuild().forTasks(task).run())
+    val extraArgs = Option(System.getProperty("esVersion")).map(v => s"-PesVersion=$v").toList
+    val result = connect.map(_.newBuild().withArguments(extraArgs*).forTasks(task).run())
     connect.map(_.close())
     result.fold(throw _, _ => ())
   }
