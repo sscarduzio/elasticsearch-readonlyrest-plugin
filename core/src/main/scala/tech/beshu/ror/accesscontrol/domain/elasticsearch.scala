ef1c7d848147be75593098936ebdf9fb1d8eaad0
mgoworko <37329559+mgoworko@users.noreply.github.com>
2026-05-09 22:34:49 +0200

    [RORDEV-1837] Handle global_checkpoint_sync as an internal action (#1244)

--- core/src/main/scala/tech/beshu/ror/accesscontrol/domain/elasticsearch.scala
+++ core/src/main/scala/tech/beshu/ror/accesscontrol/domain/elasticsearch.scala
@@ -43,6 +43,7 @@ object Action {
     val getSettingsAction: Action = EsAction("indices:monitor/settings/get")
     val monitorStateAction: Action = EsAction("cluster:monitor/state")
     val restoreSnapshotAction: Action = EsAction("cluster:admin/snapshot/restore")
+    val globalCheckpointSyncAction: Action = EsAction("indices:admin/seq_no/global_checkpoint_sync")
   }
 
   abstract sealed class RorAction(override val value: String) extends Action with EnumEntry
@@ -109,7 +110,19 @@ object Action {
       .getOrElse(EsAction(value))
   }
 
-  def isInternal(actionString: String): Boolean = actionString.startsWith("internal:")
+  def isInternal(actionString: String): Boolean =
+    actionString.startsWith("internal:") ||
+      // This operation is called by the TransportReplicationAction (optionally, controlled by syncGlobalCheckpointAfterOperation toggle).
+      // When called, it is executed directly in the calling thread's context, which means that it would go through ACL.
+      // It would have to be explicitly added to the list of allowed actions in order to work.
+      // It is treated as an internal action instead, so it does not have to be manually added to ACL rules.
+      //
+      // It can be considered as a bug, or at least a discrepancy, in ES logic:
+      //   - in a similar situation, the 'retention_lease_background_sync' action is explicitly wrapped in empty system context
+      //   - which means that action is treated correctly by default
+      //   - 'indices:admin/seq_no/global_checkpoint_sync' should be likely treated the same, but it is not, so we have to handle it here
+      actionString == EsAction.globalCheckpointSyncAction.value
+
   def isMonitorState(actionString: String): Boolean = actionString == EsAction.monitorStateAction.value
   def isXpackSecurity(actionString: String): Boolean = actionString.startsWith("cluster:admin/xpack/security/")
   def isRollupAction(actionString: String): Boolean = actionString.startsWith("cluster:admin/xpack/rollup/")
