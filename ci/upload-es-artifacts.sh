da8ac65346c8f4c48dd69ca8cf80808a6ce5ed4d
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2026-05-30 17:44:48 +0200

    [RORDEV-2060][RORDEV-2062][RORDEV-2064] ES 9.4.2, 9.3.5, 8.19.16 support (#1257)

--- ci/upload-es-artifacts.sh
+++ ci/upload-es-artifacts.sh
@@ -4,4 +4,6 @@ set -xe
 
 echo ">>> ($0) UPLOADING ES ARTIFACTS ..."
 
-#./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=9.2.2
+#./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=9.4.2
+#./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=9.3.5
+#./gradlew --stacktrace --info clean ror-tools:uploadArtifactsFromEsBinaries -PesVersion=8.19.16
