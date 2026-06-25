1e05ad167ccd768aa61856d75fb535de59e9bf12
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2026-06-25 13:52:23 +0200

    [RORDEV-2094][RORDEV-2095] ES 9.3.6, 8.19.17 support (#1277)

--- ci/upload-files-to-s3.sh
+++ ci/upload-files-to-s3.sh
@@ -9,11 +9,14 @@ fi
 
 DEST="${!#}"
 
+# Which store to upload to: ARTIFACTS (default) or LIBS. Selects ROR_<STORE>_STORE_* vars.
+STORE="${ROR_S3_TARGET_STORE:-ARTIFACTS}"
+
 for ((i = 1; i < $#; i++)); do
   FILE="${!i}"
 
-  echo "Uploading $FILE to $DEST ..."
-  upload_using_aws_s3_uploader "$FILE" "$DEST"
+  echo "Uploading $FILE to $DEST (store: $STORE) ..."
+  upload_using_aws_s3_uploader "$FILE" "$DEST" "$STORE"
 done
 
 echo "DONE"
