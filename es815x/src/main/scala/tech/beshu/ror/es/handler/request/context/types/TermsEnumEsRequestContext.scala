4de83d204fd45cebb52ae98f1ba555f41bee1420
Michał Goworko <mgw@touk.pl>
2026-08-02 14:09:07 +0200

    qs

--- es815x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/TermsEnumEsRequestContext.scala
+++ es815x/src/main/scala/tech/beshu/ror/es/handler/request/context/types/TermsEnumEsRequestContext.scala
@@ -27,7 +27,7 @@ import tech.beshu.ror.accesscontrol.domain.{ClusterIndexName, FieldLevelSecurity
 import tech.beshu.ror.accesscontrol.request.{TermsEnumRequestAdapter, TermsEnumRequestFieldsSupport}
 import tech.beshu.ror.es.handler.AclAwareRequestFilter.EsContext
 import tech.beshu.ror.es.handler.request.context.ModificationResult
-import tech.beshu.ror.es.handler.request.context.ModificationResult.Modified
+import tech.beshu.ror.es.handler.request.context.ModificationResult.{Modified, ShouldBeInterrupted}
 import tech.beshu.ror.es.handler.response.FLSContextHeaderHandler
 import tech.beshu.ror.implicits.*
 import tech.beshu.ror.syntax.*
@@ -60,19 +60,30 @@ class TermsEnumEsRequestContext(
     request.asInstanceOf[IndicesRequest].indices.asSafeSet.flatMap(RequestedIndex.fromString)
   }
 
-  // A 'filter' rule never reaches here: BaseEsRequestContext.isAllowedForDLS is false for the '_terms_enum' action,
-  // so FilterRule already rejects the match for any block that has a 'filter' rule configured.
+  // A 'filter' rule is not expected to reach here: BaseEsRequestContext.isAllowedForDLS is false for the
+  // '_terms_enum' action, so FilterRule already rejects the match for any block that has a 'filter' rule configured.
+  // There is no way to apply document level security to a '_terms_enum' request, so if that invariant ever stops
+  // holding, we have to fail closed instead of silently running the request without DLS.
   override protected def update(
       request: ActionRequest with Replaceable,
       filteredRequestedIndices: NonEmptyList[RequestedIndex[ClusterIndexName]],
       filter: Option[Filter],
       fieldLevelSecurity: Option[FieldLevelSecurity]
   ): ModificationResult = {
-    request.indices(filteredRequestedIndices.stringify: _*)
-    TermsEnumRequestFieldsSupport.fieldsRestrictionsToApply(this, fieldLevelSecurity).foreach { restrictions =>
-      FLSContextHeaderHandler.addContextHeader(threadPool, restrictions)
+    filter match {
+      case Some(_) =>
+        logger.error(
+          "a 'filter' rule was matched for the '_terms_enum' request, but document level security cannot be " +
+            "applied to it, so we have to interrupt the request processing"
+        )
+        ShouldBeInterrupted
+      case None =>
+        request.indices(filteredRequestedIndices.stringify: _*)
+        TermsEnumRequestFieldsSupport.fieldsRestrictionsToApply(this, fieldLevelSecurity).foreach { restrictions =>
+          FLSContextHeaderHandler.addContextHeader(threadPool, restrictions)
+        }
+        Modified
     }
-    Modified
   }
 
 }
